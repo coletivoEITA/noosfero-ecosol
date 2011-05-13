0529681f24dff572fe80310e11f2aed197a0526e
Hugo Melo <hugo@riseup.net>
2011-05-12 21:48:25 -0300

    Add factories for testing

---
+++ plugins/distribution/test/factories.rb
@@ -0,0 +1,19 @@
+module DistributionFactory
+
+  def factory_num_seq
+    Noosfero::Factory.num_seq
+  end
+
+  def defaults_for_distribution_delivery_method
+    { :name => 'My delivery ' + factory_num_seq.to_s }
+  end
+
+  def defaults_for_distribution_order
+    {:order_session_id => 1, :consumer_id => 1, :supplier_delivery_id => 1, :consumer_delivery_id => 1} 
+  end
+
+  def defaults_for_distribution_node
+    {:profile_id => 1, :role => 'supplier'}
+  end
+end
+
