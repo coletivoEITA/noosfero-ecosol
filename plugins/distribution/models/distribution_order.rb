5f871d166fd3bb69d0e6de6b1202c895d903b9f1
Hugo Melo <hugo@riseup.net>
2011-05-12 21:41:23 -0300

    Add validation

--- plugins/distribution/models/distribution_order.rb
+++ plugins/distribution/models/distribution_order.rb
@@ -4,4 +4,9 @@ class DistributionOrder < ActiveRecord::Base
   has_one :consumer_delivery, :class_name => 'DistributionDeliveryMethod'
   belongs_to :order_session, :class_name => 'DistributionOrderSession'
   has_many :ordered_products, :class_name => 'DistributionOrderedProducts'
+
+  validates_presence_of :consumer_delivery, :if => :is_delivery?
+
+  def is_delivery?
+    consumer_delivery.delivery_type == 'delivery'
 end
--- plugins/distribution/test/unit/distribution_node_test.rb
+++ plugins/distribution/test/unit/distribution_node_test.rb
@@ -2,22 +2,24 @@ require File.dirname(__FILE__) + '/../../../../test/test_helper'
 
 class DistributionNodeTest < Test::Unit::TestCase
 
+  def setup
+    @p = build(Profile)
+  end
+
   should 'have a profile' do
     d = DistributionNode.create
     assert !d.valid?
   end
 
   should 'create necessary roles before instance creation' do
-    p = build(Profile)
-    d = DistributionNode.create! :profile => p
+    d = build(DistributionNode, :profile => @p)
     assert_not_nil DistributionNode::Roles.consumer(d.profile.environment)
   end
 
   should 'add and remove a consumer' do
-    p1 = build(Profile)
     p2 = build(Profile)
-    d1 = DistributionNode.create! :profile => p1
-    d2 = DistributionNode.create! :profile => p2
+    d1 = build(DistributionNode, :profile => @p)
+    d2 = build(DistributionNode, :profile => p2)
     d1.add_consumer(d2)
     assert d1.consumers.include?(d2)
     #FIXME
@@ -28,9 +30,8 @@ class DistributionNodeTest < Test::Unit::TestCase
   end
 
   should 'have a product' do
-    p = build(Profile)
-    d = DistributionNode.create! :profile => p
-    p = DistributionProduct.create! :distribution_node => d
+    d = build(DistributionNode, :profile => @p)
+    p = build(DistributionProduct, :distribution_node => d)
     assert d.distribution_products.include? p
     assert p.distribution_node == d
   end
