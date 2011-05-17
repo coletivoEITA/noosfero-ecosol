0098eade014aa0636c80edeb3dfd70bcfb8384ad
Hugo Melo <hugo@riseup.net>
2011-05-16 23:02:32 -0300

    Start changes to order tests.
    Conflicts:
    
    	plugins/distribution/models/distribution_order.rb
    	plugins/distribution/test/factories.rb

--- plugins/distribution/test/unit/distribution_order_test.rb
+++ plugins/distribution/test/unit/distribution_order_test.rb
@@ -1,11 +1,8 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
 
 class DistributionOrderTest < ActiveSupport::TestCase
-  fixtures :profiles, :distribution_order_session, :distribution_node, :distribution_order, :distribution_delivery_method
-
   should 'have an order' do
-    os = distribution_order_session()
-    p os.to_yaml
+    os = build(DistributionOrderSession)
     p = build(Profile)
     d = build(DistributionNode)
     dm = build(DistributionDeliveryMethod)
