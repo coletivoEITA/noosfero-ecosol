d907d72457ad339e6c939294044bdb74d004d689
Hugo Melo <hugo@riseup.net>
2011-09-01 23:23:27 -0300

    Fixed issues on order/edit and started changes in our_products

--- plugins/distribution/public/distribution.js
+++ plugins/distribution/public/distribution.js
@@ -1,10 +1,30 @@
-function distributionRowSetSelected(event) {
-  jQuery('.value_row').removeClass('selected');
-  jQuery('.value_row').find('.ordered_product_more_actions').hide();
+function sessionProductSetFocus(id) {
+  jQuery(id).find('input').focus();
+
+}
+
+function distributionSessionRowSetSelected(event) {
+  it=jQuery(this);
+  jQuery('.value_row').find('.quantity_entry').hide();
+  jQuery('.value_row').find('.quantity_number').show();
   jQuery('.value_row').find('.session_product_more_info').hide();
-  jQuery(this).addClass('selected');
+  if (it.find('.value_row').hasClass('selected')) {
+    it.find('.value_row').removeClass('selected');
+  }
+  else {
+    jQuery('#session_column').find('.value_row').removeClass('selected');
+    it.find('.value_row').addClass('selected');
+    it.find('.session_product_more_info').show();
+    it.find('.quantity_number').hide();
+    it.find('.quantity_entry').show();
+  }
+}
+
+function distributionOrderedRowSetSelected(event) {
+  jQuery('#inside_box_order_products').find('.value_row').removeClass('selected');
+  jQuery('.value_row').find('.ordered_product_more_actions').hide();
+  jQuery(this).find('.value_row').addClass('selected');
   jQuery(this).find('.ordered_product_more_actions').show();
-  jQuery(this).find('.session_product_more_info').show();
 }
 
 var session_product_editing = jQuery();
