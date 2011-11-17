633092117d3343f7459f35008d489ab7df6397ba
Hugo Melo <hugo@riseup.net>
2011-11-17 19:01:21 -0200

    Add ODF::SpreadSheet

---
+++ vendor/plugins/rodf/lib/odf/paragraph_container.rb
@@ -0,0 +1,35 @@
+# Copyright (c) 2010 Thiago Arrais
+#
+# This file is part of rODF.
+#
+# rODF is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+
+# rODF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with rODF.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'rubygems'
+require 'builder'
+
+require 'odf/container'
+
+module ODF
+  # Container for all kinds of paragraph content
+  class ParagraphContainer < Container
+    def content_parts
+      @content_parts ||= []
+    end
+
+    def content_parts_xml
+      content_parts.map {|p| p.xml}.join
+    end
+  end
+end
+
