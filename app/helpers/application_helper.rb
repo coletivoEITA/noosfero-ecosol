83a5f2cb5bd07e116ef382c1a4b5bfef60afe5d7
Braulio Bhavamitra <brauliobo@gmail.com>
2012-10-30 14:07:15 +0000

    Avoid fetch of profile in link_to_homepage
    Conflicts:
    	app/views/region_validators/region.rhtml
    	app/views/search/_product.rhtml
    	app/views/search/_profile.rhtml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -153,7 +153,9 @@ module ApplicationHelper
   end
 
   def link_to_homepage(text, profile = nil, options = {})
-    p = if profile
+    p = if profile.is_a?(Profile)
+          profile
+        elsif profile.is_a?(String)
           Profile[profile]
         else
           user
