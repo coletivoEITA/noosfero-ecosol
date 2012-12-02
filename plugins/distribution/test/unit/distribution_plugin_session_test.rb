773e87b517555540a586f7d6bef2423a2c0344c5
Hugo Melo <hugo.melo@gmail.com>
2012-12-01 22:57:52 -0200

    Add validation test to session

--- plugins/distribution/test/unit/distribution_plugin_session_test.rb
+++ plugins/distribution/test/unit/distribution_plugin_session_test.rb
@@ -10,11 +10,19 @@ class DistributionPluginSessionTest < ActiveSupport::TestCase
 
     @node = DistributionPluginNode.create!(:profile => @profile, :role => 'collective')
     @node.products = @profile.products.map { |p| DistributionPluginSessionProduct.create!(:product => p) }
+    DistributionPluginDeliveryMethod.create! :name => 'at home', :delivery_type => 'pickup', :node => @node
     @session = DistributionPluginSession.create!(:node => @node)
   end
 
   should 'add products from node after create' do
     assert_equal @session.products.collect(&:product_id), @node.products.collect(&:id)
+  end
+
+  should 'have at least one delivery method unless in edition status' do
+    session = DistributionPluginSession.create! :node => @node, :name => "Testes batidos", :start => DateTime.now, :status => 'edition'
+    assert session
+    session.status = 'orders'
+    assert_nil session.save!
   end
 
 end
