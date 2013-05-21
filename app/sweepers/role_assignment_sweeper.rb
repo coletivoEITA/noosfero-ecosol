2bb18ccf860c8daab5e2af40be858e95378210e0
Braulio Bhavamitra <brauliobo@gmail.com>
2013-05-21 11:52:21 -0300

    Revert "Merge branch 'ai2657' of git://github.com/caioformiga/noosfero-ecosol into cirandas"
    This reverts commit 88587aee19d3e4c2f0f4a1954a44556ec447169c, reversing
    changes made to 1862427a00e28a0a77193b4430fa5c59fa6bdb11.

--- app/sweepers/role_assignment_sweeper.rb
+++ app/sweepers/role_assignment_sweeper.rb
@@ -23,7 +23,10 @@ protected
       expire_timeout_fragment(ck)
     }
 
-    expire_profile_blocks(profile.blocks)
+    profile.blocks_to_expire_cache.each { |block|
+      blocks = profile.blocks.select{|b| b.kind_of?(block)}
+      BlockSweeper.expire_blocks(blocks)
+    }
   end
 
 end
