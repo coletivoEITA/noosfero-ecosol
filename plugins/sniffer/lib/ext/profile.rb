d342f2e04d460f925407c17c6d81972e0a2d6f68
Alan Tygel <alantygel@gmail.com>
2013-06-11 11:53:39 -0300

    improvements on escambo plugin
    interests, knowledges and products bloks with exchange button, add new
    button, and individual pages for each one

--- plugins/sniffer/lib/ext/profile.rb
+++ plugins/sniffer/lib/ext/profile.rb
@@ -5,7 +5,8 @@ class Profile
   has_one :sniffer_profile, :class_name => 'SnifferPluginProfile'
   has_many :product_categories, :through => :sniffer_profile
   has_many :interests, :source => :product_categories, :through => :sniffer_profile
-
+  has_many :opportunities, :source => :opportunities, :through => :sniffer_profile
+  
   def distance
     @distance
   end
