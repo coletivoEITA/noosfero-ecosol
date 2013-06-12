821b383e000b559010f473b6a008a2123c236d54
Braulio Bhavamitra <brauliobo@gmail.com>
2013-06-12 11:24:58 -0300

    Use custom marker instead of google's marker

--- plugins/sniffer/public/sniffer.js
+++ plugins/sniffer/public/sniffer.js
@@ -7,7 +7,7 @@ _.templateSettings = {
 // from http://stackoverflow.com/questions/17033397/javascript-strings-with-keyword-parameters
 String.prototype.format = function(obj) {
   return this.replace(/%\{([^}]+)\}/g,function(_,k){ return obj[k] });
-}
+};
 
 sniffer = {
 
@@ -123,16 +123,76 @@ sniffer = {
       consumersIcon: "/plugins/sniffer/images/marker_consumers.png",
       bothIcon: "/plugins/sniffer/images/marker_both.png",
 
-      //openBalloon: mapOpenBalloon,
-      openBalloon: function (marker, html) {
-        infoBox = new InfoBox({boxStyle: {width: null}, closeBoxURL: ""});
-        InfoBox.prototype.addClickHandler_ = function () {
-          closeBox = jQuery('#close-box')[0];
-          this.closeListener_ = google.maps.event.addDomListener(closeBox, 'click', this.getCloseClickHandler_());
-        }
-        infoBox.setPosition(marker.getPosition());
-        infoBox.setContent(html);
-        infoBox.open(map, marker);
+      marker: {
+
+        //create: mapPutMarker,
+        create: function (lat, lng, title, icon, url_or_function) {
+          var point_str = lat + ":" + lng;
+          if (mapPoints[point_str]) {
+            lng += (Math.random() - 0.5) * 0.02;
+            lat += (Math.random() - 0.5) * 0.02;
+          } else {
+            mapPoints[point_str] = true;
+          }
+
+          var template = jQuery('#marker-template');
+          var element = jQuery(_.template(template.html(), {icon: icon, title: title})).get(0);
+          var point = new google.maps.LatLng(lat, lng);
+          var marker = new CustomMarker({map: map, element: element, position: point});
+
+          jQuery(marker.element).click(function() { url_or_function(marker); });
+          mapBounds.extend(point);
+
+          return marker;
+        },
+
+        add: function(profile, icon, filtered) {
+          if (filtered == undefined)
+            filtered = true;
+
+          var marker = sniffer.search.profile.findMarker(profile.id);
+          if (!marker)
+            marker = sniffer.search.map.marker.create(profile.lat, profile.lng, profile.name, icon, sniffer.search.map.balloon.fill);
+          marker.profile = profile;
+
+          if (filtered)
+            sniffer.search.map.markerList.push(marker);
+
+          return marker;
+        },
+
+        index: function (product_category_id, marker) {
+          if (sniffer.search.map.markerIndex[product_category_id] == undefined)
+            sniffer.search.map.markerIndex[product_category_id] = [];
+
+          sniffer.search.map.markerIndex[product_category_id].push(marker);
+          sniffer.search.filters.push(product_category_id);
+        },
+      },
+
+      balloon: {
+
+        //open: mapOpenBalloon,
+        open: function (marker, html) {
+          infoBox = new InfoBox({boxStyle: {width: null}, closeBoxURL: ""});
+          InfoBox.prototype.addClickHandler_ = function () {
+            closeBox = jQuery('#close-box')[0];
+            this.closeListener_ = google.maps.event.addDomListener(closeBox, 'click', this.getCloseClickHandler_());
+          }
+          infoBox.setPosition(marker.getPosition());
+          infoBox.setContent(html);
+          infoBox.open(map, marker);
+        },
+
+        fill: function (marker) {
+          if (marker.cachedData)
+            sniffer.search.map.balloon.open(marker, marker.cachedData);
+          else
+            jQuery.post(marker.profile.balloonUrl, marker.profile.balloonData, function (data) {
+              marker.cachedData = jQuery(data).html();
+              sniffer.search.map.balloon.open(marker, marker.cachedData);
+            });
+        },
       },
 
       resize: function () {
@@ -151,7 +211,7 @@ sniffer = {
 
         var profile = options.profile;
         profile.balloonUrl = options.myBalloonUrl;
-        var marker = sniffer.search.map.addMarker(profile, sniffer.search.map.homeIcon, false);
+        var marker = sniffer.search.map.marker.add(profile, sniffer.search.map.homeIcon, false);
 
         _.each(options.mapData, function (data) {
           var profile = data.profile;
@@ -169,10 +229,10 @@ sniffer = {
 
           profile.balloonUrl = options.balloonUrl.replace('_id_', profile.id);
           profile.balloonData = data;
-          var marker = sniffer.search.map.addMarker(profile, icon);
+          var marker = sniffer.search.map.marker.add(profile, icon);
 
           _.each(_.union(sp, cp), function (p) {
-            sniffer.search.map.indexMarker(p.product_category_id, marker);
+            sniffer.search.map.marker.index(p.product_category_id, marker);
           });
         });
 
@@ -180,39 +240,7 @@ sniffer = {
         mapCenter();
       },
 
-      addMarker: function(profile, icon, filtered) {
-        if (filtered == undefined)
-          filtered = true;
-
-        var marker = sniffer.search.profile.findMarker(profile.id);
-        if (!marker)
-          marker = mapPutMarker(profile.lat, profile.lng, profile.name, icon, sniffer.search.map.fillBalloon);
-        marker.profile = profile;
-
-        if (filtered)
-          sniffer.search.map.markerList.push(marker);
-
-        return marker;
-      },
-
-      indexMarker: function (product_category_id, marker) {
-        if (sniffer.search.map.markerIndex[product_category_id] == undefined)
-          sniffer.search.map.markerIndex[product_category_id] = [];
-
-        sniffer.search.map.markerIndex[product_category_id].push(marker);
-        sniffer.search.filters.push(product_category_id);
-      },
-
-      fillBalloon: function (marker) {
-        if (marker.cachedData)
-          sniffer.search.map.openBalloon(marker, marker.cachedData);
-        else
-          jQuery.post(marker.profile.balloonUrl, marker.profile.balloonData, function (data) {
-            marker.cachedData = jQuery(data).html();
-            sniffer.search.map.openBalloon(marker, marker.cachedData);
-          });
-      },
-
     },
   },
-}
+};
+
