cb8526b04c5fb209dda2edc35b5aa46e9084a963
Braulio Bhavamitra <brauliobo@gmail.com>
2013-06-26 23:01:05 -0300

    Code enterprise creation page

--- plugins/escambo/controllers/myprofile/escambo_plugin_myprofile_controller.rb
+++ plugins/escambo/controllers/myprofile/escambo_plugin_myprofile_controller.rb
@@ -5,6 +5,14 @@ class EscamboPluginMyprofileController < MyProfileController
   end
 
   def new_enterprise
+    params[:enterprise_data] ||= {}
+    @enterprise = Enterprise.new params[:enterprise_data].merge(:environment => environment)
+    if request.post?
+      @enterprise.identifier = Noosfero.convert_to_identifier @enterprise.name
+      @enterprise.save!
+      @enterprise.add_admin profile
+      redirect_to enterprise.url
+    end
   end
 
 end
