9481ea9041b56922930887c5a20ad5e3d2febc7f
Braulio Bhavamitra <brauliobo@gmail.com>
2013-07-10 17:33:48 -0300

    Copy code from exchange plugin

--- plugins/escambo/lib/escambo_plugin/escambo_display_helper.rb
+++ plugins/escambo/lib/escambo_plugin/escambo_display_helper.rb
@@ -4,7 +4,8 @@ module EscamboPlugin::EscamboDisplayHelper
 
   # image that can be aligned, centered, and resized with aspect ratio
   def profile_link_with_image profile, size=:portrait
-     link_to '', profile.url, :class => "profile-image #{size}", :style => "background-image: url(#{profile_icon profile, size})"
+    link = link_to '', profile.url, :class => "inner", :style => "background-image: url(#{profile_icon profile, size})"
+    content_tag 'div', link, :class => "profile-image #{size}"
   end
 
 end
