f193ff0b639febcb0bed77026c8b6960fca6e119
Caio Formiga <caio.formiga@gmail.com>
2013-07-18 17:19:26 -0300

    Fixing recipients enterpeise mail contact nil

--- plugins/shopping_cart/lib/shopping_cart_plugin/mailer.rb
+++ plugins/shopping_cart/lib/shopping_cart_plugin/mailer.rb
@@ -19,7 +19,16 @@ class ShoppingCartPlugin::Mailer < Noosfero::Plugin::MailerBase
 
   def supplier_notification(customer, supplier, items, delivery_option)
     domain = supplier.environment.default_hostname
-    recipients    supplier.contact_email
+    contact_email = []
+    if supplier.contact_email.blank?
+      supplier.admins do |admin|
+        puts 'admins e-mail: %s' % admin.email
+        contact_email << admin.email
+      end
+    else
+      contact_email << supplier.contact_email
+    end
+    recipients    contact_email
     from          'no-reply@' + domain
     reply_to      customer[:email]
     subject       _("[%s] You have a new buy request from %s.") % [supplier.environment.name, customer[:name]]
