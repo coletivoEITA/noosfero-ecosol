e0e7eb061a4d36730f5f0dc5d0cffa9bda3b1503
Braulio Bhavamitra <braulio@eita.org.br>
2013-09-30 10:58:26 -0300

    Remove old distribution plugin

--- plugins/distribution/db/migrate/history/20121215141934_add_header_type_to_distribution_plugin_node.rb
+++
@@ -1,9 +0,0 @@
-class AddHeaderTypeToDistributionPluginNode < ActiveRecord::Migration
-  def self.up
-    add_column :distribution_plugin_nodes, :header_type, :string
-  end
-
-  def self.down
-    remove_column :distribution_plugin_nodes, :header_type
-  end
-end
