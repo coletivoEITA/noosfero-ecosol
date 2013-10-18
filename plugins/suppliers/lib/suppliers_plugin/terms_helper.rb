f6e42a3d6798ca765256107d1ca906ec8104eda7
Braulio Bhavamitra <brauliobo@gmail.com>
2013-10-18 11:09:52 -0300

    Fix use of format on older systems

--- plugins/suppliers/lib/suppliers_plugin/terms_helper.rb
+++ plugins/suppliers/lib/suppliers_plugin/terms_helper.rb
@@ -1,6 +1,8 @@
 module SuppliersPlugin::TermsHelper
 
   Terms = [:consumer, :supplier]
+  # '.' ins't supported by the % format function (altought it works on some newer systems)
+  TermsSeparator = '_'
   TermsVariations = [:singular, :plural]
   TermsTransformations = [:capitalize]
   TermsKeys = Terms.map do |term|
@@ -9,24 +11,36 @@ module SuppliersPlugin::TermsHelper
 
   protected
 
-  def translated_terms keys = TermsKeys, transformations = TermsTransformations
-    @translated_terms ||= {}
+  def sub_separator str
+    str.gsub '.', TermsSeparator
+  end
+
+  def sub_separator_items str
+    str.gsub!(/\%\{[^\}]*\}/){ |x| sub_separator x }
+    str
+  end
+
+  def translated_terms keys = TermsKeys, transformations = TermsTransformations, sep = TermsSeparator
+    @translated_terms ||= HashWithIndifferentAccess.new
     return @translated_terms unless @translated_terms.blank?
 
     @terms_context ||= 'suppliers_plugin'
     keys.each do |key|
       translation = I18n.t "#{@terms_context}.terms.#{key}"
-      @translated_terms["terms.#{key}"] = translation
+      new_key = sub_separator key
+      @translated_terms["terms#{sep}#{new_key}"] = translation
 
       transformations.map do |transformation|
-        @translated_terms["terms.#{key}.#{transformation}"] = translation.send transformation
+        @translated_terms["terms#{sep}#{new_key}#{sep}#{transformation}"] = translation.send transformation
       end
     end
     @translated_terms
   end
 
   def t key, options = {}
-    I18n.t(key, options) % translated_terms
+    translation = I18n.t key, options
+    sub_separator_items translation
+    translation % translated_terms
   end
 
 end
