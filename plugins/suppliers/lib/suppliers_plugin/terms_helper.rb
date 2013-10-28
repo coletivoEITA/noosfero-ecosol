fd291261ff096d5e44cfeb093ba0e85740bb9d0a
Braulio Bhavamitra <braulio@eita.org.br>
2013-10-28 20:09:40 -0300

    Support auxiliaries in TermsHelper

--- plugins/suppliers/lib/suppliers_plugin/terms_helper.rb
+++ plugins/suppliers/lib/suppliers_plugin/terms_helper.rb
@@ -1,50 +1,88 @@
 module SuppliersPlugin::TermsHelper
 
-  Terms = [:consumer, :supplier]
   # '.' ins't supported by the % format function (altought it works on some newer systems)
-  TermsSeparator = '_'
-  TermsVariations = [:singular, :plural]
-  TermsAuxiliars = [nil, :article, :undefined_article, :from, :this, :from_this, :by, :your, :by_your, :by_article]
-  TermsTransformations = [:capitalize]
-  TermsKeys = Terms.map do |term|
-    TermsVariations.map do |variation|
-      TermsAuxiliars.map do |auxiliar|
-        [term, variation, auxiliar].compact.join('.')
+  # FIXME remove when a newer ruby/system is required as it affects performance
+  Separator = '_'
+  I18nSeparator = '.'
+
+  Terms = [:consumer, :supplier]
+  Auxiliars = [
+    nil,
+    :it, :to, :article, :undefined_article, :which, :from, :this, :by, :your,
+    :to_it, :from_this, :from_which, :from_which_article, :by_your, :by_article,
+    # adjectives
+    :none, :own, :new,
+    :by_own, :new_undefined_article
+  ]
+  Variations = [:singular, :plural]
+  Transformations = [:capitalize]
+
+  # FORMAT: terms.term.auxiliar.variation.transformation
+  Keys = Terms.map do |term|
+    Auxiliars.map do |auxiliar|
+      Variations.map do |variation|
+        [term, auxiliar, variation].compact.join I18nSeparator
       end
     end
   end.flatten
 
+  @translations = HashWithIndifferentAccess.new
+  def self.translations
+    @translations
+  end
+
+  @cache = {}
+  def self.cache
+    @cache
+  end
+
   protected
 
+  def self.included base
+    base.alias_method_chain :translate, :cache
+    base.send :alias_method, :t, :translate
+  end
+
   def sub_separator str
-    str.gsub '.', TermsSeparator
+    str.gsub I18nSeparator, Separator
   end
   def sub_separator_items str
     str.gsub!(/\%\{[^\}]*\}/){ |x| sub_separator x }
     str
   end
 
-  def translated_terms keys = TermsKeys, transformations = TermsTransformations, sep = TermsSeparator
-    @translated_terms ||= HashWithIndifferentAccess.new
-    return @translated_terms unless @translated_terms.blank?
+  def translated_terms keys = Keys, translations = SuppliersPlugin::TermsHelper.translations, transformations = Transformations, sep = Separator
+    translated_terms ||= (translations[I18n.locale] ||= HashWithIndifferentAccess.new)
+    return translated_terms if translated_terms.present?
 
+    # may be replaced on context (e.g. controller)
     @terms_context ||= 'suppliers_plugin'
-    keys.each do |key|
-      translation = I18n.t "#{@terms_context}.terms.#{key}"
-      new_key = sub_separator key
-      @translated_terms["terms#{sep}#{new_key}"] = translation
 
+    keys.each do |key|
+      translation = I18n.t! "#{@terms_context}.terms.#{key}" rescue next
+
+      processed_key = sub_separator key
+
+      translated_terms["terms#{sep}#{processed_key}"] = translation
       transformations.map do |transformation|
-        @translated_terms["terms#{sep}#{new_key}#{sep}#{transformation}"] = translation.send transformation
+        translated_terms["terms#{sep}#{processed_key}#{sep}#{transformation}"] = translation.send transformation
       end
     end
-    @translated_terms
+    translated_terms
   end
 
-  def t key, options = {}
+  def translate key, options = {}
     translation = I18n.t key, options
     sub_separator_items translation
     translation % translated_terms
+  end
+
+  def translate_with_cache key, options = {}
+    cache = (SuppliersPlugin::TermsHelper.cache[I18n.locale] ||= {})
+    hit = cache[key]
+    return hit if hit.present?
+
+    cache[key] = translate_without_cache key, options
   end
 
 end
