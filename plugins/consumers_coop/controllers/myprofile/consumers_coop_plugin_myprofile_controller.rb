db79d6c559387e6a119607590087683b47eccd53
Braulio Bhavamitra <braulio@eita.org.br>
2013-10-30 15:07:17 -0300

    Remove tasks with problems

--- plugins/consumers_coop/controllers/myprofile/consumers_coop_plugin_myprofile_controller.rb
+++ plugins/consumers_coop/controllers/myprofile/consumers_coop_plugin_myprofile_controller.rb
@@ -18,7 +18,7 @@ class ConsumersCoopPluginMyprofileController < MyProfileController
 
   def settings
     if params[:commit]
-      was_enabled = profile.consumers_coop_settings.enabled?
+      was_enabled = profile.consumers_coop_settings.enabled
 
       params[:profile_data].delete(:consumers_coop_settings).each do |attr, value|
         profile.consumers_coop_settings.send "#{attr}=", value
@@ -26,7 +26,7 @@ class ConsumersCoopPluginMyprofileController < MyProfileController
       profile.update_attributes! params[:profile_data]
       profile.consumers_coop_header_image_save
 
-      if !was_enabled and profile.consumers_coop_settings.enabled?
+      if !was_enabled and profile.consumers_coop_settings.enabled
         profile.consumers_coop_enable
       elsif was_enabled
         profile.consumers_coop_disable
