f8c151ae5a9a0f5056ce2d88a35c944fbcea6e72
Braulio Bhavamitra <braulio@eita.org.br>
2014-02-23 16:15:41 -0300

    Add a set of monkey patches to ActionMailer
    - Add a default mailer layout in app/views/layouts
    - Support rendering partials
    - Set host on urls automically be settings the environment
    - Inline CSS with premailer

--- lib/noosfero/core_ext.rb
+++ lib/noosfero/core_ext.rb
@@ -3,3 +3,5 @@ require 'noosfero/core_ext/integer'
 require 'noosfero/core_ext/array'
 require 'noosfero/core_ext/object'
 require 'noosfero/core_ext/active_record'
+require 'noosfero/core_ext/action_mailer'
+
