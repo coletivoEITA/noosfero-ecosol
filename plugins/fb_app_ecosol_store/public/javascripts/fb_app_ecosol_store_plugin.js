9d3e5d8a04b3e2c3e0312dd3746a49ec693b9fa7
Vinicius Cubas Brand <viniciuscb@gmail.com>
2014-03-20 17:42:29 -0300

    possibilidade de escolher vitrine de produtos por query ou por empreendimentos

--- plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
+++ plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
@@ -31,10 +31,61 @@ fb_app_ecosol_store = {
     document.getElementsByTagName('head')[0].appendChild(script);
   },
 
+  init_admin: function() {
+    if (jQuery('#fb_store_admin_page').length > 0) {
+        this.init_integration_type_selection();
+        this.autocomplete();
+        this.redraw_tabela_empreendimentos();
+    }
+  },
+
+  init_integration_type_selection: function() {
+      var func_on_radio_change;
+
+      func_on_radio_change = function(evt) {
+        var radio_elements = jQuery('[name=fb_integration_type]')
+
+        radio_elements.each(function(key,elm) {
+            if (elm.checked) {
+                jQuery('#fb_integration_type_'+elm.value).show()
+            } else {
+                jQuery('#fb_integration_type_'+elm.value).hide()
+            }
+        })
+      };
+
+      jQuery('[name=fb_integration_type]').each(function(key,radio_elm) {
+        var $radio_elm = jQuery(radio_elm)
+        $radio_elm.on('change',func_on_radio_change);
+        $radio_elm.trigger('change')
+      })
+  },
+
+  redraw_tabela_empreendimentos: function() {
+      var i, tbody, tr_html, tr, remove_button, self;
+      self = this;
+      tbody = jQuery('#fb_tabela_empreendimentos')
+      tbody.empty()
+      for (i=0;i<empreendimentos.length;i++) {
+          tr_html  = '<tr>'
+          tr_html += '<td>'+empreendimentos[i].name+'</td>'
+          tr_html += '<td class="remove-btn-holder"></td>'
+          tr_html += '</tr>'
+          tr = jQuery(tr_html);
+          tbody.append(tr);
+          remove_button = jQuery('<button data-profile-pos="'+i+'">Remover</button>');
+          remove_button.on('click',function(evt) {
+            var pos = parseInt(jQuery(evt.target).data('profile-pos'))
+            empreendimentos.splice(pos,1);
+            self.redraw_tabela_empreendimentos();
+          });
+          tr.find('.remove-btn-holder').append(remove_button);
+      }
+  }
 };
 
 jQuery('document').ready(function(){
-  fb_app_ecosol_store.autocomplete();
+  fb_app_ecosol_store.init_admin();
 });
 
 fb_app_ecosol_store.addJS('http://dtygel.eita.org.br/lojacirandasfb/utils.js');
