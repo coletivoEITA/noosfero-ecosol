94e966cb32e4f891e736f87ddabc6cc2ea029caf
Braulio Bhavamitra <braulio@eita.org.br>
2014-03-20 12:04:33 -0300

    Update javascript and stylesheet

--- plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
+++ plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
@@ -1,26 +1,41 @@
-jQuery('document').ready(function(){
-    alert('blabla');
-  jQuery('[name=fb_store]').autocomplete({
-    source: function(request,response){
-      jQuery.ajax('/plugin/fb_app_ecosol_store/search',{
-        data: {
-          query: request
-        },
+fb_app_ecosol_store = {
 
-        dataType: 'json',
-        type: 'GET',
-        success: function(data) {
-          var i, names;
-          names = [];
-          for (i in data) {
-            names.push(data[i]['name'])
+  autocomplete: function() {
+    jQuery('[name=fb_store]').autocomplete({
+      source: function(request,response){
+        jQuery.ajax('/plugin/fb_app_ecosol_store/search',{
+          data: {
+            query: request
+          },
+
+          dataType: 'json',
+          type: 'GET',
+          success: function(data) {
+            var i, names;
+            names = [];
+            for (i in data) {
+              names.push(data[i]['name'])
+            }
+            response(names)
+          },
+          error: function() {
+            alert('Erro!')
           }
-          response(names)
-        },
-        error: function() {
-          alert('Erro!')
-        }
-      });
-    }
-  });
+        });
+      }
+    });
+  },
+
+  addJS: function(js) {
+    var script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'url';
+    document.getElementsByTagName('head')[0].appendChild(script);
+  },
+
+};
+
+jQuery('document').ready(function(){
+  fb_app_ecosol_store.autocomplete();
 });
+
+fb_app_ecosol_store.addJS('http://dtygel.eita.org.br/lojacirandasfb/utils.js');
+
