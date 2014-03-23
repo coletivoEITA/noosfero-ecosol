2630e233898c5d219060bff2116299d4ba999abc
Braulio Bhavamitra <braulio@eita.org.br>
2014-03-23 15:23:48 -0300

    Move FB.init to theme

--- plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
+++ plugins/fb_app_ecosol_store/public/javascripts/fb_app_ecosol_store_plugin.js
@@ -146,23 +146,6 @@ fb_app_ecosol_store = {
     init: function(id, next) {
       this.id = id;
       this.page_tab_next = next;
-
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : id,
-          status     : true,
-          xfbml      : true
-        });
-      };
-
-      (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-
     },
 
     add_tab: function () {
