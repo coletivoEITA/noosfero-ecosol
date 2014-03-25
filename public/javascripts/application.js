a56e2b7c88b19ca6bed80c9dc4463f9a4ae9f0dd
Braulio Bhavamitra <braulio@eita.org.br>
2014-03-24 23:06:24 -0300

    Send more data on user_data

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,6 +9,9 @@ function noosfero_init() {
   // focus_first_field(); it is moving the page view when de form is down.
 }
 
+noosfero = {
+};
+
 /* If applicable, find the first field in which the user can type and move the
  * keyboard focus to it.
  *
@@ -510,6 +513,10 @@ function new_qualifier_row(selector, select_qualifiers, delete_button) {
   jQuery(selector).append("<tr><td>" + select_qualifiers + "</td><td id='certifier-area-" + index + "'><select></select>" + delete_button + "</td></tr>");
 }
 
+// override this to take action after user_data load
+function customUserDataCallback() {
+};
+
 // controls the display of the login/logout stuff
 jQuery(function($) {
   $.ajaxSetup({
@@ -520,6 +527,8 @@ jQuery(function($) {
   });
 
   $.getJSON('/account/user_data', function userDataCallBack(data) {
+    noosfero.user_data = data;
+    customUserDataCallback();
     if (data.login) {
       // logged in
       if (data.chat_enabled) {
