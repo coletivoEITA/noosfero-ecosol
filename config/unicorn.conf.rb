700fd54bff85a2ccd7f133c60c799cc067400d94
Braulio Bhavamitra <braulio@eita.org.br>
2014-04-27 16:37:23 -0300

    Parametrize unicorn configuration

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,18 +1,32 @@
-rails_root = File.expand_path "#{File.dirname __FILE__}/.."
-pids_dir = "#{rails_root}/tmp/pids"
-port = 50000
+RailsRoot = File.expand_path "#{File.dirname __FILE__}/.."
+PidsDir = "#{RailsRoot}/tmp/pids"
+ListenAddress = "127.0.0.1"
+ListenPort = 50000
+Workers = 10
+Timeout = 30
+Backlog = 2048
 
-working_directory rails_root
+WarmUpUrl = '/'
 
-worker_processes 12
-timeout 30
+KillByRequests = 500..600
+KillByMemory = 160..256
+GcFrequency = 20
 
-#stderr_path "#{rails_root}/log/unicorn.stderr.log"
-#stdout_path "#{rails_root}/log/unicorn.stdout.log"
-pid "#{pids_dir}/unicorn.pid"
+WorkerListen = true
+WorkerPidFile = true
 
-listen "#{rails_root}/tmp/unicorn.sock", :backlog => 64
-listen port, :tcp_nopush => true
+
+working_directory RailsRoot
+
+worker_processes Workers
+timeout Timeout
+
+#stderr_path "#{RailsRoot}/log/unicorn.stderr.log"
+#stdout_path "#{RailsRoot}/log/unicorn.stdout.log"
+pid "#{PidsDir}/unicorn.pid"
+
+listen "#{RailsRoot}/tmp/unicorn.sock", :backlog => Backlog
+listen "#{ListenAddress}:#{ListenPort}", :tcp_nopush => true
 
 # combine Ruby 2 or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
@@ -26,8 +40,8 @@ before_fork do |server, worker|
   # a .oldbin file exists if unicorn was gracefully restarted with a USR2 signal
   # we should terminate the old process now that we're up and running
   Thread.new do
-    sleep 30
-    old_pid = "#{pids_dir}/unicorn.pid.oldbin"
+    sleep Timeout*2
+    old_pid = "#{PidsDir}/unicorn.pid.oldbin"
     if File.exists? old_pid
       begin
         Process.kill "QUIT", File.read(old_pid).to_i
@@ -50,27 +64,40 @@ after_fork do |server, worker|
 
   require 'unicorn/worker_killer'
   # Max requests per worker
-  #max_request_min = 500
-  #max_request_max = 600
-  #use Unicorn::WorkerKiller::MaxRequests, max_request_min, max_request_max
+  max_request_min = KillByRequests.begin
+  max_request_max = KillByRequests.end
+  middleware.use Unicorn::WorkerKiller::MaxRequests, max_request_min, max_request_max
   # Max memory size (RSS) per worker
-  oom_min = 192 * (1024**2)
-  oom_max = 256 * (1024**2)
+  oom_min = KillByMemory.begin * (1024**2)
+  oom_max = KillByMemory.end * (1024**2)
   middleware.use Unicorn::WorkerKiller::Oom, oom_min, oom_max
+
+  # say to the kernel to kill very big workers first than other processes
+  File.open("/proc/#{Process.pid}/oom_adj", "w"){ |f| f << '12' }
 
   require_dependency 'unicorn/oob_gc'
   # Don't run GC during requests
   # FIXME: this makes the worker too big and activate Unicorn::WorkerKiller::Oom
   #GC.disable
-  GC_FREQUENCY = 20
-  middleware.use Unicorn::OobGC, GC_FREQUENCY
+  middleware.use Unicorn::OobGC, GcFrequency
 
-  # make a request warming up this new worker
-  Thread.new do
-    require 'rack/test'
-    request = Rack::MockRequest.new server.app
-    request.get "http://0.0.0.0:#{port}/"
+  if WarmUpUrl
+    # make a request warming up this new worker
+    Thread.new do
+      require 'rack/test'
+      request = Rack::MockRequest.new server.app
+      request.get "http://#{ListenAddress}:#{ListenPort}#{WarmUpUrl}"
+    end
   end
 
+  if WorkerListen
+    # per-process listener ports for debugging/admin/migrations
+    server.listen "#{ListenAddress}:#{ListenPort + worker.nr}", :tries => -1, :delay => 5
+  end
+
+  if WorkerPidFile
+    child_pid_file = server.config[:pid].sub '.pid', ".#{worker.nr}.pid"
+    system "echo #{Process.pid} > #{child_pid_file}"
+  end
 end
 
