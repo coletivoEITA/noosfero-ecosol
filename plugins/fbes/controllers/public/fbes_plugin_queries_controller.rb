5f1588aa440f482b23bcbb49ea3ced8afab5a7e1
Braulio Bhavamitra <braulio@eita.org.br>
2014-06-03 13:04:48 -0300

    Don't overwrite string

--- plugins/fbes/controllers/public/fbes_plugin_queries_controller.rb
+++ plugins/fbes/controllers/public/fbes_plugin_queries_controller.rb
@@ -8,13 +8,13 @@ class FbesPluginQueriesController < PublicController
   FbesPlugin::Queries::Hash.each do |name, query|
     define_method name do
       page = (params[:page] || 1).to_i
-      per_page = (params[:per_page] || 20).to_i
+      per_page = if params[:per_page] == 'all' then nil else (params[:per_page] || 20).to_i end
       format = params[:format]
       request.format = format.to_sym if format.present?
 
-      query = "#{query} offset #{(page-1)*per_page} limit #{per_page}"
+      query_with_pagination = if not per_page then query else "#{query} offset #{(page-1)*per_page} limit #{per_page}" end
       result = ActiveRecord::Base.transaction do
-        ActiveRecord::Base.connection.execute query
+        ActiveRecord::Base.connection.execute query_with_pagination
       end
 
       # make ordered hashes. FIXME: remove with ruby > 1.9
@@ -36,12 +36,16 @@ class FbesPluginQueriesController < PublicController
             csv << result.first.keys
             result.each{ |r| csv << r.values }
           end
-          render :text => csv
+          send_csv csv
         end
       end
     end
   end
 
   protected
+
+  def send_csv csv
+    send_data csv, :type => 'text/csv; charset=utf-8; header=present', :disposition => "attachment; filename=#{params[:action]}.csv"
+  end
 
 end
