6b6758c9230796d6b47b2f5117e5a817a909969f
Braulio Bhavamitra <braulio@eita.org.br>
2014-06-03 12:47:19 -0300

    Add new queries and order fields

--- plugins/fbes/controllers/public/fbes_plugin_queries_controller.rb
+++ plugins/fbes/controllers/public/fbes_plugin_queries_controller.rb
@@ -14,13 +14,23 @@ class FbesPluginQueriesController < PublicController
         ActiveRecord::Base.connection.execute query
       end
 
+      # make ordered hashes. FIXME: remove with ruby > 1.9
+      new_result = result.map do |record|
+        new_record = ActiveSupport::OrderedHash.new
+        result.fields.each do |key|
+          new_record[key] = record[key]
+        end
+        new_record
+      end
+      result = new_result
+
       respond_to do |format|
         format.json do
           render :json => result.to_json
         end
         format.csv do
           csv = FasterCSV.generate do |csv|
-            csv << result.fields
+            csv << result.first.keys
             result.each{ |r| csv << r.values }
           end
           render :text => csv
--- plugins/fbes/lib/fbes_plugin/queries.rb
+++ plugins/fbes/lib/fbes_plugin/queries.rb
@@ -1,93 +1,128 @@
 class FbesPlugin::Queries
 
   Hash = {
-    :orders_by_month =>
-      "select to_char(created_at,'YYYY-MM') mes, count(*)
-        from orders_plugin_orders
-        group by mes
-        order by mes desc",
-    :enterprises_updated_products =>
-      "create temporary table tmp as
-        select profile_id, to_char(updated_at,'YYYY-MM') mes, count(*) qtde
-          from products
-          group by mes, profile_id
-          order by mes desc, profile_id;
+    :orders_by_month => <<EOQ,
+select to_char(created_at,'YYYY-MM') mes, count(*)
+  from orders_plugin_orders
+  group by mes
+  order by mes desc
+EOQ
 
-        select a.mes, a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, a.qtde
-          from tmp a, profiles b
-          where a.profile_id=b.id and b.type='Enterprise' and visible=true and active=true
-          order by a.mes desc, a.qtde desc",
-    :enterprises_products_ranking =>
-      "create temporary table tmp as
-          select profile_id, count(*) qtde from products group by profile_id order by qtde desc;
+    :enterprises_with_products => <<EOQ,
+select a.id, a.enabled as "ativado?", case when a.nickname!='' then a.name||' - '||a.nickname else a.name end nome, 'http://cirandas.net/'||a.identifier site, a.address "endereco", a.contact_phone tel, to_char(a.updated_at, 'dd/mm/yyyy') as "Ultima atualizacaoo", array_to_string(ARRAY (select b.name from products as b where b.profile_id=a.id), ', ') as produtos from profiles as a where a.type='Enterprise' and a.active is true and a.visible is true order by a.updated_at desc
+EOQ
 
-      select a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, a.qtde from tmp a, profiles b
-          where a.profile_id=b.id and b.type='Enterprise' and visible=true and active=true
-          order by qtde desc",
-    :enterprises_orders_ranking =>
-      "create temporary table tmp as
-          select a.id order_id, a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
-              from orders_plugin_orders a, profiles b
-              where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+    :enterprises_enabled_with_products => <<EOQ,
+select a.id, case when a.nickname!='' then a.name||' - '||a.nickname else a.name end nome, 'http://cirandas.net/'||a.identifier site, a.address "endereco", a.contact_phone tel, to_char(a.updated_at, 'dd/mm/yyyy') as "Ultima atualizacao", array_to_string(ARRAY (select b.name from products as b where b.profile_id=a.id), ', ') as produtos from profiles as a where a.type='Enterprise' and a.active is true and a.visible is true and a.enabled is true order by a.updated_at desc
+EOQ
 
-      select t.profile_id, b.name, b.url, t.qtde, t.total_pedidos
-          from (select profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
-                  from tmp
-                  group by profile_id
-              ) t
-          join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
-          order by t.qtde desc",
-    :enterprises_orders_quantity_by_month =>
-      "create temporary table tmp as
-          select a.id order_id, a.profile_id, a.created_at, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
-              from orders_plugin_orders a, profiles b
-              where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+    :enterprises_updated_products => <<EOQ,
+create temporary table tmp as
+  select profile_id, to_char(updated_at,'YYYY-MM') mes, count(*) qtde
+    from products
+    group by mes, profile_id
+    order by mes desc, profile_id;
 
-      select t.mes, t.profile_id, b.name, b.url, t.qtde, t.total_pedidos
-          from (select to_char(created_at,'YYYY-MM') mes, profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
-                  from tmp
-                  group by mes, profile_id
-              ) t
-          join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
-          order by t.mes desc, t.qtde desc",
-    :enterprises_quantity_with_orders_by_month =>
-      "create temporary table tmp as
-          select a.id order_id, a.profile_id, a.created_at, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
-              from orders_plugin_orders a, profiles b
-              where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+  select a.mes, a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, a.qtde
+    from tmp a, profiles b
+    where a.profile_id=b.id and b.type='Enterprise' and visible=true and active=true
+    order by a.mes desc, a.qtde desc
+EOQ
 
-      select t.mes, count(*) qtde_ees, sum(t.qtde) qtde_pedidos, sum(t.total_pedidos) valor_total_pedidos
-          from (select to_char(created_at,'YYYY-MM') mes, profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
-                            from tmp
-                            group by mes, profile_id
-                        ) t
-          join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
-          group by t.mes
-          order by t.mes desc",
-    :users_created_by_month =>
-      "select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
-        from profiles
-        where type='Person' group by mes
-        order by mes desc",
-    :communities_created_by_month =>
-      "select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
-        from profiles
-        where type='Community'
-        group by mes
-        order by mes desc",
-    :comments_created_by_month =>
-      "select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
-        from comments
-        where spam is not true and source_type='Article'
-        group by mes
-        order by mes desc",
-    :contents_most_commented =>
-      "select t.source_id, a.name, a.type, a.created_at, t.qtde_comentarios
-        from (select source_id, count(*) qtde_comentarios
-                     from comments
-                     where spam is not true and source_type='Article'
-                     group by source_id
-                 ) t join articles a on a.id=t.source_id
-        order by qtde_comentarios desc",
+    :enterprises_products_ranking => <<EOQ,
+create temporary table tmp as
+    select profile_id, count(*) qtde from products group by profile_id order by qtde desc;
+
+select a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, a.qtde from tmp a, profiles b
+    where a.profile_id=b.id and b.type='Enterprise' and visible=true and active=true
+    order by qtde desc
+EOQ
+
+    :enterprises_orders_ranking => <<EOQ,
+create temporary table tmp as
+    select a.id order_id, a.profile_id, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
+        from orders_plugin_orders a, profiles b
+        where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+
+select t.profile_id, b.name, b.url, t.qtde, t.total_pedidos
+    from (select profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
+            from tmp
+            group by profile_id
+        ) t
+    join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
+    order by t.qtde desc
+EOQ
+
+    :enterprises_orders_quantity_by_month => <<EOQ,
+create temporary table tmp as
+    select a.id order_id, a.profile_id, a.created_at, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
+        from orders_plugin_orders a, profiles b
+        where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+
+select t.mes, t.profile_id, b.name, b.url, t.qtde, t.total_pedidos
+    from (select to_char(created_at,'YYYY-MM') mes, profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
+            from tmp
+            group by mes, profile_id
+        ) t
+    join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
+    order by t.mes desc, t.qtde desc",
+EOQ
+
+    :enterprises_quantity_with_orders_by_month => <<EOQ,
+create temporary table tmp as
+    select a.id order_id, a.profile_id, a.created_at, b.name, 'http://cirandas.net/'||b.identifier url, (select sum(i.price) from orders_plugin_items i where i.order_id=a.id) valor_dos_pedidos
+        from orders_plugin_orders a, profiles b
+        where a.profile_id=b.id and b.type='Enterprise' and b.visible=true and b.active=true;
+
+select t.mes, count(*) qtde_ees, sum(t.qtde) qtde_pedidos, sum(t.total_pedidos) valor_total_pedidos
+    from (select to_char(created_at,'YYYY-MM') mes, profile_id, count(*) qtde, sum(valor_dos_pedidos) total_pedidos, max(order_id) m
+                      from tmp
+                      group by mes, profile_id
+                  ) t
+    join tmp b on b.profile_id=t.profile_id and b.order_id=t.m
+    group by t.mes
+    order by t.mes desc
+EOQ
+
+    :users_created_by_month => <<EOQ,
+select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
+  from profiles
+  where type='Person' group by mes
+  order by mes desc
+EOQ
+
+    :communities_created_by_month => <<EOQ,
+select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
+  from profiles
+  where type='Community'
+  group by mes
+  order by mes desc
+EOQ
+
+    :comments_created_by_month => <<EOQ,
+select to_char(created_at, 'YYYY-MM') mes, count(*) qtde
+  from comments
+  where spam is not true and source_type='Article'
+  group by mes
+  order by mes desc",
+EOQ
+
+    :communities => <<EOQ,
+select a.id, a.name nome, 'http://cirandas.net/'||a.identifier site, a.members_count "Qtde integrantes", to_char(a.created_at, 'dd/mm/yyyy') as "Data de criacao" from profiles as a where a.type='Community' and visible is true order by a.created_at desc
+EOQ
+
+    :people => <<EOQ,
+select a.id, a.name nome, a.nickname apelido, b.email, 'http://cirandas.net/'||a.identifier site, a.address "endereco", a.contact_phone tel, to_char(a.updated_at, 'dd/mm/yyyy') as "Ultima atualizacao" from profiles as a, users as b where a.type='Person' and a.user_id = b.id order by a.updated_at desc
+EOQ
+
+    :contents_most_commented => <<EOQ,
+select t.source_id, a.name, a.type, a.created_at, t.qtde_comentarios
+  from (select source_id, count(*) qtde_comentarios
+               from comments
+               where spam is not true and source_type='Article'
+               group by source_id
+           ) t join articles a on a.id=t.source_id
+  order by qtde_comentarios desc
+EOQ
   }
 end
