fdb0d43b499e4414a891cd46c67f40cfe7712cc1
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-17 15:22:54 -0300

    Add update themes script

---
+++ update-themes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update colivre themes
+cd public/designs/themes/cirandas-themes/colivre-themes
+git pull --rebase;
+
+cd ..
+git commit colivre-themes -m "Update colivre"
+cd ../../../..
+git commit public/designs -m 'Update themes'
+
+# push all
+g pssm
+
