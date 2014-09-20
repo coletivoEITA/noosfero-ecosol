1efb6dc73f0bfacc1221c8430934f5b8ca70bd43
Hugo Melo <hugo@riseup.net>
2014-09-20 20:41:03 -0300

    Fix weird issue on has_many association with polymorphic

--- plugins/delivery/models/delivery_plugin/option.rb
+++ plugins/delivery/models/delivery_plugin/option.rb
@@ -1,5 +1,8 @@
 class DeliveryPlugin::Option < Noosfero::Plugin::ActiveRecord
 
+  # FIXME: some unknown code is overwriting this to orders_cycle_plugin_cycles
+  set_table_name 'delivery_plugin_options'
+
   belongs_to :delivery_method, :class_name => 'DeliveryPlugin::Method'
   belongs_to :owner, :polymorphic => true
 
