f68b0334c38425fab840bd30c52f2acdc24b5043
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-24 14:08:51 -0300

    Remove monkey patches on render

--- lib/noosfero/core_ext/action_mailer.rb
+++ lib/noosfero/core_ext/action_mailer.rb
@@ -2,27 +2,6 @@ class ActionMailer::Base
 
   attr_accessor :environment
 
-  # Monkey patch to:
-  # 1) Allow multiple render calls, by keeping the body assigns into an instance variable
-  # 2) Don't render layout for partials
-  def render opts={}
-    @assigns ||= opts.delete(:body)
-    if opts[:file] && (opts[:file] !~ /\// && !opts[:file].respond_to?(:render))
-      opts[:file] = "#{mailer_name}/#{opts[:file]}"
-    end
-
-    begin
-      old_template, @template = @template, initialize_template_class(@assigns || {})
-      unless opts[:partial]
-        layout = respond_to?(:pick_layout, true) ? pick_layout(opts) : false
-        opts.merge! :layout => layout
-      end
-      @template.render opts
-    ensure
-      @template = old_template
-    end
-  end
-
   # Set default host automatically if environment is set
   def url_for options = {}
     options[:host] ||= environment.default_hostname if self.environment
