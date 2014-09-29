d2a255dedbf18234e798cd3fa5e2f113dda02dee
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-29 16:16:25 -0300

    Set timezone

--- config/local.rb
+++ config/local.rb
@@ -3,6 +3,7 @@ FastGettext.locale = Noosfero.default_locale
 FastGettext.default_locale = Noosfero.default_locale
 I18n.locale = Noosfero.default_locale
 I18n.default_locale = Noosfero.default_locale
+Time.zone = 'America/Sao_Paulo'
 
 require 'noosfero/terminology'
 class CirandasTerminology < Noosfero::Terminology::Custom
