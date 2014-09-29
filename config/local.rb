eea3899d06146142f17c7aaef54d8669ba777b7a
Braulio Bhavamitra <braulio@eita.org.br>
2014-09-29 16:17:37 -0300

    Set locale and timezone

---
+++ config/local.rb
@@ -0,0 +1,7 @@
+Noosfero.default_locale = 'pt'
+FastGettext.locale = Noosfero.default_locale
+FastGettext.default_locale = Noosfero.default_locale
+I18n.locale = Noosfero.default_locale
+I18n.default_locale = Noosfero.default_locale
+
+Time.zone = 'America/Sao_Paulo'
