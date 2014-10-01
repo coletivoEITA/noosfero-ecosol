7064737ec4d0675c1c106ddcf766feef17fe4eb0
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-01 09:00:17 -0300

    Update to tinymce 4
    Conflicts:
    	app/views/shared/tiny_mce.rhtml

---
+++ public/javascripts/tinymce.js
@@ -0,0 +1,21 @@
+
+noosfero.tinymce = {
+
+  defaultOptions: {
+    mode: "textareas", //necessary for compat3x
+    theme: "modern",
+    relative_urls: false,
+    remove_script_host: false,
+    extended_valid_elements: "applet[style|archive|codebase|code|height|width],comment,iframe[src|style|allowtransparency|frameborder|width|height|scrolling],embed[title|src|type|height|width],audio[controls|autoplay],video[controls|autoplay],source[src|type]",
+    entity_encoding: 'raw',
+    setup: function(editor) {
+      tinymce_macros_setup(editor)
+    },
+  },
+
+  init: function(_options) {
+    var options = jQuery.extend({}, this.defaultOptions, _options)
+    tinymce.init(options);
+    jQuery('.mceEditor').tinymce(options);
+  },
+};
