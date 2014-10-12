7f9e5fcb7b153a20886b555a0fe9551ee2b5e1be
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-11 22:24:20 -0300

    Load locales inside plugins' path

--- config/application.rb
+++ config/application.rb
@@ -126,6 +126,8 @@ module Noosfero
       :key    => '_noosfero_session',
     }
 
+    config.i18n.load_path += Dir.glob "#{Rails.root}/{baseplugins,config/plugins/*}/locales/*.{rb,yml}"
+
     Noosfero::Plugin.setup(config)
 
   end
