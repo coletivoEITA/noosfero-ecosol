78e41de85eb6976cbe7ee2fed208b0a6e9d9a6e4
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-20 22:57:45 -0300

    Remove cache_* options support

--- config/initializers/01_load_config.rb
+++ config/initializers/01_load_config.rb
@@ -1,6 +1,3 @@
 file = Rails.root.join('config', 'noosfero.yml')
 NOOSFERO_CONF = File.exists?(file) ? YAML.load_file(file)[Rails.env] || {} : {}
 
-NOOSFERO_CONF['cache_stylesheets'] = true if NOOSFERO_CONF['cache_stylesheets'].nil?
-NOOSFERO_CONF['cache_javascripts'] = true if NOOSFERO_CONF['cache_javascripts'].nil?
-
