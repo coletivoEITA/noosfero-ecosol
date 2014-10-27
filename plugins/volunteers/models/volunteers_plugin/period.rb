06e9b3312cd1627145fd18d93e836412cd585035
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-26 22:59:09 -0300

    Destroy dependent

--- plugins/volunteers/models/volunteers_plugin/period.rb
+++ plugins/volunteers/models/volunteers_plugin/period.rb
@@ -10,7 +10,7 @@ class VolunteersPlugin::Period < ActiveRecord::Base
 
   belongs_to :owner, polymorphic: true
 
-  has_many :assignments, class_name: 'VolunteersPlugin::Assignment', foreign_key: :period_id, include: [:profile]
+  has_many :assignments, class_name: 'VolunteersPlugin::Assignment', foreign_key: :period_id, include: [:profile], dependent: :destroy
 
   validates_presence_of :owner
   validates_presence_of :name
