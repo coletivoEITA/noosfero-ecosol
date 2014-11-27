6f6004e55381b635c22a4063671bae6286530951
Braulio Bhavamitra <braulio@eita.org.br>
2014-11-26 22:01:41 -0300

    Revert "Ignore symlinks of themes"
    This reverts commit b5bdd2b41878506351ba4aa526f15d178d919f35.

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -45,7 +45,6 @@ class Theme
 
     def approved_themes(owner)
       Dir.glob(File.join(Rails.root, 'public', self.system_themes_dir, '*')).map do |item|
-        next if File.symlink? item
         next unless File.exists? File.join(item, 'theme.yml')
         id = File.basename item
         config = YAML.load_file File.join(item, 'theme.yml')
@@ -139,7 +138,7 @@ class Theme
   end
 
   def ==(other)
-    other.id == self.id
+    other.is_a?(self.class) && (other.id == self.id)
   end
 
   def private_copy profile
