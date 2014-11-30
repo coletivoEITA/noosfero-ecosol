f964c1464d0e2f163c8f09d6f00468d78e440c62
Hugo Melo <hugo@riseup.net>
2014-11-30 19:18:41 -0200

    Fix search

--- plugins/suppliers/controllers/myprofile/suppliers_plugin_product_controller.rb
+++ plugins/suppliers/controllers/myprofile/suppliers_plugin_product_controller.rb
@@ -29,6 +29,22 @@ class SuppliersPluginProductController < MyProfileController
     end
   end
 
+  def search
+    @supplier = SuppliersPlugin::Supplier.find_by_id params[:supplier_id] if params[:supplier_id].present?
+
+    SuppliersPlugin::DistributedProduct.send :with_exclusive_scope do
+      scope = profile.distributed_products.unarchived.joins([:from_products, :suppliers])
+      @products = SuppliersPlugin::BaseProduct.search_scope(scope, params).paginate per_page: 10, page: params[:page], order: 'from_products_products.name ASC'
+      @products_count = SuppliersPlugin::BaseProduct.search_scope(scope, params).count
+    end
+    @product_categories = Product.product_categories_of @products
+    @new_product = SuppliersPlugin::DistributedProduct.new
+    @new_product.profile = profile
+    @new_product.supplier = @supplier
+    @units = Unit.all
+    render partial: 'suppliers_plugin_product/search'
+  end
+
   def edit
     @product = SuppliersPlugin::DistributedProduct.find params[:id]
     @product.update_attributes params["product_#{@product.id}"]
