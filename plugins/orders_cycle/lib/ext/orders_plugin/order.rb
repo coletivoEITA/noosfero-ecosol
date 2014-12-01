f34b18843380082950e2925cee67b340f631f30f
Braulio Bhavamitra <braulio@eita.org.br>
2014-11-30 23:35:32 -0300

    Fix purchases generation

--- plugins/orders_cycle/lib/ext/orders_plugin/order.rb
+++ plugins/orders_cycle/lib/ext/orders_plugin/order.rb
@@ -15,24 +15,24 @@ class OrdersPlugin::Order
     self.cycles = [cycle]
   end
 
-  has_many :offered_products, :through => :items, :source => :offered_product, :uniq => true
-  has_many :distributed_products, :through => :offered_products, :source => :from_products, :uniq => true
-  has_many :supplier_products, :through => :distributed_products, :source => :from_products, :uniq => true
+  has_many :offered_products, through: :items, source: :offered_product, uniq: true
+  has_many :distributed_products, through: :offered_products, source: :from_products, uniq: true
+  has_many :supplier_products, through: :distributed_products, source: :from_products, uniq: true
 
-  has_many :suppliers, :through => :supplier_products, :uniq => true
+  has_many :suppliers, through: :supplier_products, uniq: true
 
-  has_many :from_products, :through => :products
-  has_many :to_products, :through => :products
+  has_many :from_products, through: :products
+  has_many :to_products, through: :products
 
   extend CodeNumbering::ClassMethods
-  code_numbering :code, :scope => (proc do
+  code_numbering :code, scope: (proc do
     if self.cycle then self.cycle.send(self.orders_name) else self.profile.orders end
   end)
 
   def code_with_cycle
     return self.code_without_cycle unless self.cycle
     I18n.t('orders_cycle_plugin.lib.ext.orders_plugin.order.cyclecode_ordercode') % {
-      :cyclecode => self.cycle.code, :ordercode => self['code']
+      cyclecode: self.cycle.code, ordercode: self['code']
     }
   end
   alias_method_chain :code, :cycle
