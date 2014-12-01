c77e395c89d8cd809a38acd8c66663637eb11b5d
Hugo Melo <hugo@riseup.net>
2014-12-01 02:13:20 -0200

    Fix url_for on actionmailer

--- lib/noosfero/core_ext/action_mailer.rb
+++ lib/noosfero/core_ext/action_mailer.rb
@@ -1,11 +1,20 @@
+module Noosfero
+  module ActionMailer
+    module UrlHelper
+
+      # Set default host automatically if environment is set
+      def url_for options = {}
+        options[:host] ||= environment.default_hostname if environment
+        super options
+      end
+    end
+  end
+end
+
 class ActionMailer::Base
 
   attr_accessor :environment
 
-  # Set default host automatically if environment is set
-  def url_for options = {}
-    options[:host] ||= environment.default_hostname if self.environment
-    super
-  end
+  helper Noosfero::ActionMailer::UrlHelper
 
 end
