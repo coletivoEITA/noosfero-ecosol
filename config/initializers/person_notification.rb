31992e70919b4b2d76e71626ee2bd3b7e6fcada1
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-02 19:21:10 -0300

    Disable person network notification

--- config/initializers/person_notification.rb
+++ config/initializers/person_notification.rb
@@ -1,4 +1,6 @@
 if Delayed::Backend::ActiveRecord::Job.table_exists? &&
   Delayed::Backend::ActiveRecord::Job.attribute_names.include?('queue')
-  PersonNotifier.schedule_all_next_notification_mail
+
+  # FIXME: Disable it as is hanging delayed_job a lot on restarts
+  #PersonNotifier.schedule_all_next_notification_mail
 end
