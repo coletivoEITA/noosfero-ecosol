2c0827ac5efb07ebb04bb43656ae6f08ff4c65b9
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-06 20:04:49 -0300

    Load plugins in alphabetical order

--- lib/noosfero/plugin.rb
+++ lib/noosfero/plugin.rb
@@ -124,7 +124,7 @@ class Noosfero::Plugin
     def available_plugins
       unless @available_plugins
         path = File.join(Rails.root, '{baseplugins,config/plugins}', '*')
-        @available_plugins = Dir.glob(path).select{ |i| File.directory?(i) }
+        @available_plugins = Dir.glob(path).sort.select{ |i| File.directory?(i) }
         if Rails.env.test? && !@available_plugins.include?(File.join(Rails.root, 'config', 'plugins', 'foo'))
           @available_plugins << File.join(Rails.root, 'plugins', 'foo')
         end
