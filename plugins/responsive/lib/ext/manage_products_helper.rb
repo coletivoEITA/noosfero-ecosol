86eb8b4b8d359f4d26bae7536186d1eacc241dc8
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-06 22:03:05 -0300

    Move responsive style to core

--- plugins/responsive/lib/ext/manage_products_helper.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-require_dependency 'manage_products_helper'
-
-module ManageProductsHelper
-  def select_unit(object)
-    collection_select(object.class.name.downcase, :unit_id, environment.units, :id, :singular, {:include_blank => _('Select the unit')}, {:class => 'form-control'})
-  end
-end
