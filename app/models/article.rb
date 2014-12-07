coletivoEITA/noosfero

Use no_feeds and latest scopes from Article

app/models/article.rb
@@ -390,6 +390,8 @@ class Article < ActiveRecord::Base
   named_scope :galleries, :conditions => { :type => 'Gallery' }
   named_scope :images, :conditions => { :is_image => true }
   named_scope :text_articles, :conditions => [ 'articles.type IN (?)', text_article_types ]
+  named_scope :no_feeds, :conditions => ["type != 'RssFeed'"]
+  named_scope :lastest, :order => "updated_at DESC"
 
   named_scope :more_comments, :order => "comments_count DESC"
   named_scope :more_views, :order => "hits DESC"

coletivoEITA/noosfero-ecosol

Conflicts:
	app/models/article.rb
	app/views/search/_blog.rhtml
	app/views/search/_folder.rhtml
	app/views/search/_forum.rhtml
	app/views/search/_image.rhtml

app/models/article.rb
@@ -465,6 +465,8 @@ class Article < ActiveRecord::Base
   scope :images, :conditions => { :is_image => true }
   scope :text_articles, :conditions => [ 'articles.type IN (?)', text_article_types ]
   scope :with_types, lambda { |types| { :conditions => [ 'articles.type IN (?)', types ] } }
+  scope :no_feeds, :conditions => ["type != 'RssFeed'"]
+  scope :latest, :order => "updated_at DESC"
 
   scope :more_popular, :order => 'hits DESC'
   scope :more_comments, :order => "comments_count DESC"