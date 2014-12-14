f53dae0777754c11c931e41cbf4289701bf978ff
Hugo Melo <hugo@riseup.net>
2014-12-14 11:30:06 -0200

    Fix delivery methods edition/creation process
     * Fixes #6

--- plugins/delivery/public/javascripts/delivery.js
+++ plugins/delivery/public/javascripts/delivery.js
@@ -2,7 +2,12 @@
 delivery = {
 
   view_toggle: function () {
+    if (jQuery('#delivery-method-edit').is(":visible"))
+      jQuery('#delivery-method-edit form.edit_delivery_method').find('textarea, input[type=text]').val('');
     jQuery('#delivery-method-choose, #delivery-method-edit').toggle();
   },
+  new_toggle: function() {
+    jQuery('#delivery-method-choose, #delivery-method-new').toggle();
+  }
 
 };
