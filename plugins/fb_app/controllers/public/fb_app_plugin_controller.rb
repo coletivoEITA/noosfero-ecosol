7f1676dceca10f3fba94a1ea5f1764b0567fcd49
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-15 16:22:22 -0300

    Redirect for unlogged users

--- plugins/fb_app/controllers/public/fb_app_plugin_controller.rb
+++ plugins/fb_app/controllers/public/fb_app_plugin_controller.rb
@@ -6,8 +6,11 @@ class FbAppPluginController < PublicController
   end
 
   def myprofile_config
-    return unless user
-    redirect_to controller: :fb_app_plugin_myprofile, profile: user.identifier
+    if logged_in?
+      redirect_to controller: :fb_app_plugin_myprofile, profile: user.identifier
+    else
+      redirect_to controller: :account, action: :login, return_to: url_for(controller: :fb_app_plugin, action: :myprofile_config)
+    end
   end
 
   protected
