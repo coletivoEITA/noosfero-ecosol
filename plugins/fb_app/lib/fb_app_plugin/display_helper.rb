0810244ac1c4d796420f05a705f5388821c73327
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-15 06:51:02 -0300

    Simple method override

--- plugins/fb_app/lib/fb_app_plugin/display_helper.rb
+++ plugins/fb_app/lib/fb_app_plugin/display_helper.rb
@@ -2,13 +2,7 @@ module FbAppPlugin::DisplayHelper
 
   protected
 
-  def self.included base
-    base.send :include, ActionView::Helpers::UrlHelper
-    base.alias_method_chain :link_to, :target
-    base.alias_method_chain :link_to_product, :iframe
-  end
-
-  def link_to_product_with_iframe product, opts = {}
+  def link_to_product product, opts = {}
     url_opts = opts.delete(:url_options) || {}
     url_opts.merge controller: :fb_app_plugin, product_id: product.id
     url = params.merge url_opts
@@ -16,10 +10,10 @@ module FbAppPlugin::DisplayHelper
       opts.merge(target: '')
   end
 
-  def link_to_with_target name = nil, options = nil, html_options = nil, &block
+  def link_to name = nil, options = nil, html_options = nil, &block
     html_options ||= {}
     html_options[:target] ||= '_parent'
-    link_to_without_target name, options, html_options, &block
+    super
   end
 
 end
