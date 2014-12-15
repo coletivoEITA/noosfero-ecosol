4ce8b26a9632592ba598a78d969fbbd668437f03
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-15 16:05:01 -0300

    Fix domain for link_to_product

--- plugins/fb_app/lib/fb_app_plugin/display_helper.rb
+++ plugins/fb_app/lib/fb_app_plugin/display_helper.rb
@@ -5,6 +5,9 @@ module FbAppPlugin::DisplayHelper
   def link_to_product product, opts = {}
     url_opts = opts.delete(:url_options) || {}
     url_opts.merge! controller: :fb_app_plugin_page_tab, product_id: product.id
+    domain = FbAppPlugin.config[:app][:domain]
+    url_opts[:host] = domain if domain.present?
+
     url = params.merge url_opts
     link_to content_tag('span', product.name), url,
       opts.merge(target: '')
