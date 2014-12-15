1f86517cc71d198d89e8db69543d0764e2fd0899
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-15 16:08:08 -0300

    Keep protocol on links

--- plugins/fb_app/lib/fb_app_plugin/display_helper.rb
+++ plugins/fb_app/lib/fb_app_plugin/display_helper.rb
@@ -7,6 +7,7 @@ module FbAppPlugin::DisplayHelper
     url_opts.merge! controller: :fb_app_plugin_page_tab, product_id: product.id
     domain = FbAppPlugin.config[:app][:domain]
     url_opts[:host] = domain if domain.present?
+    url_opts[:protocol] = '//'
 
     url = params.merge url_opts
     link_to content_tag('span', product.name), url,
@@ -15,6 +16,7 @@ module FbAppPlugin::DisplayHelper
 
   def link_to name = nil, options = nil, html_options = nil, &block
     html_options ||= {}
+    options[:protocol] = '//'
     html_options[:target] ||= '_parent'
     super
   end
