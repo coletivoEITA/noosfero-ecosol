d8bd62b55be46f92c4dfba83a9e21a1f8fa242d5
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-17 16:11:23 -0300

    Remove debug

--- plugins/fb_app/controllers/public/fb_app_plugin_page_tab_controller.rb
+++ plugins/fb_app/controllers/public/fb_app_plugin_page_tab_controller.rb
@@ -66,7 +66,6 @@ class FbAppPluginPageTabController < FbAppPluginController
     if request.put? and @page_id.present?
       create_page_tabs if @page_tab.nil?
 
-      raise params[:page_tab].inspect
       @page_tab.update_attributes! params[:page_tab]
 
       respond_to do |format|
