e60b7e482fcf682096b4d6e31aad9198e0324230
Hugo Melo <hugo@riseup.net>
2014-12-19 20:23:45 -0200

    Refactor to remove global methods

--- plugins/volunteers/lib/split_datetime.rb
+++ plugins/volunteers/lib/split_datetime.rb
@@ -1,63 +1,66 @@
-def __split_nil_time
-  Time.parse "#{Time.now.hour}:0:0"
-end
-def __split_nil_date
-  Date.today
-end
-
-def __split_to_time datetime
-  datetime = __split_nil_time if datetime.blank?
-  datetime.to_formatted_s :time
-end
-def __split_to_date datetime
-  datetime = __split_nil_date if datetime.blank?
-  datetime.strftime '%d/%m/%Y'
-end
-def __split_set_time datetime, value
-  value = if value.blank?
-    __split_nil_time
-  elsif value.kind_of? String
-    Time.parse value
-  else
-    value.to_time
-  end
-  datetime = __split_nil_date if datetime.blank?
-
-  Time.mktime(datetime.year, datetime.month, datetime.day, value.hour, value.min, value.sec).to_datetime
-end
-def __split_set_date datetime, value
-  value = if value.blank?
-    __split_nil_date
-  elsif value.kind_of? String
-    DateTime.strptime value, '%d/%m/%Y'
-  else
-    value.to_time
-  end
-  datetime = __split_nil_time if datetime.blank?
-
-  Time.mktime(value.year, value.month, value.day, datetime.hour, datetime.min, datetime.sec).to_datetime
-end
 
 module SplitDatetime
+
+  class << self
+    def nil_time
+      Time.parse "#{Time.now.hour}:0:0"
+    end
+    def nil_date
+      Date.today
+    end
+
+    def to_time datetime
+      datetime = self.nil_time if datetime.blank?
+      datetime.to_formatted_s :time
+    end
+    def to_date datetime
+      datetime = self.nil_date if datetime.blank?
+      datetime.strftime '%d/%m/%Y'
+    end
+    def set_time datetime, value
+      value = if value.blank?
+                self.nil_time
+              elsif value.kind_of? String
+                Time.parse value
+              else
+                value.to_time
+              end
+      datetime = self.nil_date if datetime.blank?
+
+      Time.mktime(datetime.year, datetime.month, datetime.day, value.hour, value.min, value.sec).to_datetime
+    end
+    def set_date datetime, value
+      value = if value.blank?
+                self.nil_date
+              elsif value.kind_of? String
+                DateTime.strptime value, '%d/%m/%Y'
+              else
+                value.to_time
+              end
+      datetime = nil_time if datetime.blank?
+
+      Time.mktime(value.year, value.month, value.day, datetime.hour, datetime.min, datetime.sec).to_datetime
+    end
+  end
 
   module SplitMethods
 
     def split_datetime attr
       define_method "#{attr}_time" do
         datetime = send attr
-        __split_to_time datetime
+        SplitDatetime.to_time datetime
       end
       define_method "#{attr}_date" do
         datetime = send attr
-        __split_to_date datetime
+        SplitDatetime.to_date datetime
       end
       define_method "#{attr}_time=" do |value|
         datetime = send attr
-        send "#{attr}=", __split_set_time(datetime, value)
+        send "#{attr}=", SplitDatetime.set_time(datetime, value)
       end
       define_method "#{attr}_date=" do |value|
         datetime = send attr
-        send "#{attr}=", __split_set_date(datetime, value)
+        send "#{attr}=", SplitDatetime.set_date(datetime, value)
       end
     end
 
