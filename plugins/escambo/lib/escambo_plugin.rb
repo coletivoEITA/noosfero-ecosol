0924c48d6fd0dd3c12b9fb5d8a5f166144114ed3
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-22 00:05:26 -0300

    Add dependencies

--- plugins/escambo/lib/escambo_plugin.rb
+++ plugins/escambo/lib/escambo_plugin.rb
@@ -1,10 +1,3 @@
-#require_dependency 'cms_learning_plugin'
-#require_dependency 'currency_plugin'
-#require_dependency 'evaluation_plugin'
-#require_dependency 'exchange_plugin'
-#require_dependency 'sniffer_plugin'
-#require_dependency 'solr_plugin'
-
 class EscamboPlugin < Noosfero::Plugin
 
   def self.plugin_name
