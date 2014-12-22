bfc4ae1740c620f7bcb056916680608b482761c9
Braulio Bhavamitra <braulio@eita.org.br>
2014-12-21 23:33:24 -0300

    Shorten long index names

--- db/migrate/20140708123314_index_role_assignments_filtered_fields.rb
+++ db/migrate/20140708123314_index_role_assignments_filtered_fields.rb
@@ -2,10 +2,10 @@ class IndexRoleAssignmentsFilteredFields < ActiveRecord::Migration
 
   def self.up
     add_index :role_assignments, [:accessor_id, :accessor_type]
-    add_index :role_assignments, [:accessor_id, :accessor_type, :role_id]
+    add_index :role_assignments, [:accessor_id, :accessor_type, :role_id], name: 'index_role_assigments_accessor_id_type_role_id'
     add_index :role_assignments, [:resource_id, :resource_type]
-    add_index :role_assignments, [:resource_id, :resource_type, :role_id]
-    add_index :role_assignments, [:accessor_id, :accessor_type, :resource_id, :resource_type]
+    add_index :role_assignments, [:resource_id, :resource_type, :role_id], name: 'index_role_assigments_resource_id_type_role_id'
+    add_index :role_assignments, [:accessor_id, :accessor_type, :resource_id, :resource_type], name: 'index_role_assigments_resource_id_type_accessor_id_type'
     add_index :profiles, [:type]
     add_index :profiles, [:visible]
     add_index :profiles, [:enabled]
