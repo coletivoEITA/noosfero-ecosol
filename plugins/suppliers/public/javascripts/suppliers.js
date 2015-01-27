659dc81ba87c08a81fc0e067c14f6ff49a14765c
Hugo Melo <hugo@riseup.net>
2015-01-27 15:11:48 -0200

    Add changes to suppliers page new design

--- plugins/suppliers/public/javascripts/suppliers.js
+++ plugins/suppliers/public/javascripts/suppliers.js
@@ -114,5 +114,116 @@ suppliers = {
     },
   },
 
+  supplier: {
+    select: {
+      all: function() {
+        jQuery('#suppliers-page input.entity-cb').attr('checked', true)
+      },
+      none: function() {
+        jQuery('#suppliers-page input.entity-cb').attr('checked', false)
+      },
+
+      activate: function(state) {
+        var selection = jQuery('#suppliers-page input.entity-cb:checked').parents('.our-product')
+        selection.find('.available input[type=checkbox]').each(function() {
+          this.checked = state
+          jQuery(this.form).submit()
+        });
+      },
+    },
+
+    toggle_open: function(event) {
+      event.preventDefault();
+      t = jQuery(event.target);
+      t.parents('.entity').toggleClass('open');
+      t.parent().find('.action-show').toggle();
+      t.parent().find('.action-hide').toggle();
+    },
+
+    toggle_edit: function(event) {
+      event.preventDefault();
+      t = jQuery(event.target);
+      t.parents('.entity').toggleClass('edit');
+    },
+
+    cancel_edition: function(event) {
+      event.preventDefault();
+      t = jQuery(event.target);
+      t.parents('.entity').removeClass('edit');
+    },
+
+    create_tokens: function(type, input) {
+      if (input == undefined)
+        input = $(this);
+      q = jQuery(input).parents('.qualifiers');
+      view = q.find('.view');
+      text = input.value;
+      input.value = '';
+
+      text = suppliers.supplier.filter_new_input(text, view);
+      text.each(function(el) {
+        el = el.strip();
+        div = jQuery('<div class=token>').appendTo(view);
+        jQuery('<input type="hidden" name="qualifiers[]">').appendTo(div).value = el;
+        jQuery('<span>').text(el).appendTo(div);
+        jQuery('<a href="#" class="remove-token">').appendTo(div).text("X");
+      });
+    },
+
+    filter_new_input: function(qualifiers, view) {
+      q = [];
+      qualifiers.split(',').each(function(qualifier){
+        qualifier.strip();
+        if (qualifier) {
+          // check if we already have that qualifier
+          have = false;
+          view.find('span').each(function(i,span){
+            if (jQuery(span).text().strip() == qualifier) {
+              have = true;
+              return;
+            }
+          });
+          if (!have)
+            q.push(qualifier);
+        }
+      });
+      return q;
+    },
+
+    remove_tokens: function(event) {
+      event.preventDefault();
+      $(this).parents('div.token').remove();
+    }
+  }
 };
+
+// EVENTS
+jQuery(document).ready(function() {
+
+  jQuery('#suppliers-page .entity a.actions-circle').click(suppliers.supplier.toggle_open);
+  jQuery('#suppliers-page .entity a.action-button.edit-button').click(suppliers.supplier.toggle_edit);
+  jQuery('#suppliers-page .entity a.action-button.cancel-button').click(suppliers.supplier.cancel_edition);
+
+  // page load first create of qualifiers tokens
+  jQuery('#suppliers-page .entity .qualifiers input.qualifier').each(function(i,el){ suppliers.supplier.create_tokens('qualifier', el)});
+  jQuery('#suppliers-page .entity .qualifiers input.qualifier').keypress(function(event) {
+    if (event.which == 13 || event.which ==  44) {// enter or comma
+      event.preventDefault();
+      suppliers.supplier.create_tokens('qualifier', $(this));
+    }
+  });
+
+  // page load first create of qualifiers tokens
+  jQuery('#suppliers-page .entity .tags input.tag').each(function(i,el){ suppliers.supplier.create_tokens('tag', el)});
+  jQuery('#suppliers-page .entity .tags input.tag').keypress(function(event) {
+    if (event.which == 13 || event.which ==  44) {// enter or comma
+      event.preventDefault();
+      suppliers.supplier.create_tokens('tag', $(this));
+    }
+  });
+
+  jQuery('#suppliers-page .entity .qualifiers a.remove-token').click(suppliers.supplier.remove_tokens);
+  jQuery('#suppliers-page .entity .tags       a.remove-token').click(suppliers.supplier.remove_tokens);
+
+});
 
