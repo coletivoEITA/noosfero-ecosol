bb5a5205ab297354cca54cb25cafe45cb5b3d569
Braulio Bhavamitra <braulio@eita.org.br>
2015-02-07 18:14:16 -0300

    Fallback locales with coutries

--- lib/noosfero/i18n.rb
+++ lib/noosfero/i18n.rb
@@ -42,3 +42,52 @@ hosted_environments.uniq.each do |env|
     FastGettext.add_text_domain(env, :type => :chain, :chain => [FastGettext::TranslationRepository.build('environment', :type => 'po', :path => custom_locale_dir)] + repos)
   end
 end
+
+# Setup fallbacks
+
+class FastGettext::TranslationRepository::Base
+  attr_accessor :files
+end
+class FastGettext::MoFile
+  attr_accessor :data
+end
+module FastGettext
+  @@fallbacks = {
+    'en_US' => 'en',
+    'pt_BR' => 'pt',
+    'fr_FR' => 'fr',
+    'hy_AM' => 'hy',
+    'de_DE' => 'de',
+    'ru_RU' => 'ru',
+    'es_ES' => 'es',
+    'it_IT' => 'it',
+  }
+
+  def self.fallbacks
+    @@fallbacks
+  end
+end
+
+FastGettext.translation_repositories.each do |domain, repository|
+  repository.chain.each do |chain|
+    new_files = {}
+
+    files = chain.files
+    files.each do |locale, file|
+      FastGettext.fallbacks.each do |dest, source|
+        next if locale != source
+
+        mo_file = files[dest]
+        unless mo_file
+          mo_file ||= FastGettext::MoFile.empty
+          new_files[dest] = mo_file
+        end
+
+        mo_file.data = file.data.merge mo_file.data
+      end
+    end
+
+    files.merge! new_files
+  end
+end
+
