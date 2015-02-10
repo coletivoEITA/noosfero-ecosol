04d162610eac3188b12c5cb101f51af071402785
Braulio Bhavamitra <braulio@eita.org.br>
2015-02-10 07:38:19 -0300

    Move stylesheets from fb_app to open_graph

--- plugins/open_graph/lib/open_graph_plugin.rb
+++ plugins/open_graph/lib/open_graph_plugin.rb
@@ -13,6 +13,10 @@ class OpenGraphPlugin < Noosfero::Plugin
     ['open_graph.js'].map{ |j| "javascripts/#{j}" }
   end
 
+  def stylesheet?
+    true
+  end
+
 end
 
 ActiveSupport.run_load_hooks :open_graph_plugin, OpenGraphPlugin
