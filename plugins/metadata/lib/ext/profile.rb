35fb0b6e6a68f2fe78a7b53cc2d8f4b3f1a07b8f
Braulio Bhavamitra <braulio@eita.org.br>
2015-03-13 23:05:49 -0300

    metadata: fix og:title on profiles

--- plugins/metadata/lib/ext/profile.rb
+++ plugins/metadata/lib/ext/profile.rb
@@ -5,7 +5,7 @@ class Profile
   metadata_spec namespace: :og, tags: {
     type: proc{ |p, plugin| plugin.context.params[:og_type] || MetadataPlugin.og_types[:profile] || :profile },
     image: proc{ |p, plugin| "#{p.environment.top_url}#{p.image.public_filename}" if p.image },
-    title: proc{ |p, plugin| p.nickname || p.name },
+    title: proc{ |p, plugin| if p.nickname.present? then p.nickname else p.name end },
     url: proc{ |p, plugin| plugin.og_url_for plugin.og_profile_url(p) },
     description: proc{ |p, plugin| p.description },
 	  updated_time: proc{ |p, plugin| p.updated_at.iso8601 },
