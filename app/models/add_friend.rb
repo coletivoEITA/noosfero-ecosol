ee83a09eabcfed622ff3f9991a71bbaf83539bc2
Braulio Bhavamitra <braulio@eita.org.br>
2015-03-16 21:24:07 -0300

    add_friend: fix validation and test

--- app/models/add_friend.rb
+++ app/models/add_friend.rb
@@ -4,6 +4,8 @@ class AddFriend < Task
 
   validates_presence_of :requestor_id, :target_id
 
+  validates_uniqueness_of :target_id, scope: [ :requestor_id, :status ], if: proc{ |t| t.status == Task::Status::ACTIVE }
+
   validates_length_of :group_for_person, :group_for_friend, :maximum => 150, :allow_nil => true
 
   alias :person :requestor
