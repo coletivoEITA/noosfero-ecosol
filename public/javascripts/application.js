46c7c06974b23f862c3ee9fa0b90c22bfc430b35
Rodrigo Souto <rodrigo@colivre.coop.br>
2015-03-24 14:34:49 -0300

    chat: focus on window when notification clicked

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1117,6 +1117,7 @@ function notifyMe(title, options) {
   }
 
   setTimeout(function() {notification.close()}, 5000);
+  notification.onclick = function(){ window.focus()};
 
   return notification;
   // At last, if the user already denied any notification, and you
