1dd2c0a180f1122128dda0170e1dcf879543b4c6
Braulio Bhavamitra <braulio@eita.org.br>
2015-04-06 18:46:11 -0300

    orders_cycle: move code from consumers_coop

--- plugins/orders_cycle/lib/ext/profile.rb
+++ plugins/orders_cycle/lib/ext/profile.rb
@@ -2,15 +2,15 @@ require_dependency 'profile'
 
 class Profile
 
-  has_many :orders_cycles, :class_name => 'OrdersCyclePlugin::Cycle', :dependent => :destroy, :order => 'created_at DESC',
-    :conditions => ["orders_cycle_plugin_cycles.status <> 'new'"]
-  has_many :orders_cycles_without_order, :class_name => 'OrdersCyclePlugin::Cycle',
-    :conditions => ["orders_cycle_plugin_cycles.status <> 'new'"]
+  has_many :orders_cycles, class_name: 'OrdersCyclePlugin::Cycle', dependent: :destroy, order: 'created_at DESC',
+    conditions: ["orders_cycle_plugin_cycles.status <> 'new'"]
+  has_many :orders_cycles_without_order, class_name: 'OrdersCyclePlugin::Cycle',
+    conditions: ["orders_cycle_plugin_cycles.status <> 'new'"]
 
-  has_many :offered_products, :class_name => 'OrdersCyclePlugin::OfferedProduct', :order => 'products.name ASC'
+  has_many :offered_products, class_name: 'OrdersCyclePlugin::OfferedProduct', order: 'products.name ASC'
 
   def orders_cycles_closed_date_range
-    list = self.orders_cycles.closing.all :order => 'start ASC'
+    list = self.orders_cycles.closing.all order: 'start ASC'
     return DateTime.now..DateTime.now if list.blank?
     list.first.start.to_date..list.last.finish.to_date
   end
