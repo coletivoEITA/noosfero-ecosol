f7a1cfa33f2376f3318a3e337fb91173890a50d0
Hugo Melo <hugo@riseup.net>
2015-04-25 08:46:18 -0300

    suppliers: move name change

--- plugins/suppliers/lib/suppliers_plugin/import.rb
+++ plugins/suppliers/lib/suppliers_plugin/import.rb
@@ -28,22 +28,22 @@ class SuppliersPlugin::Import
     puts header
     rows.each do |row|
       supplier_name = row[0].to_s.squish
-      product_name = row[1].to_s.squish
+      product_name = row[1].to_s.squish.gsub('"', '&quot;')
       product_unit = row[2].to_s.squish
       product_price = row[3].to_s.squish
 
       product_unit = consumer.environment.units.find_by_singular product_unit
 
       data[supplier_name] ||= []
-      data[supplier_name] << {:name => product_name, :unit => product_unit, :price => product_price, :product_category => product_category}
+      data[supplier_name] << {name: product_name, unit: product_unit, price: product_price, product_category: product_category}
     end
 
     data.each do |supplier_name, products|
       supplier = consumer.suppliers.find_by_name supplier_name
-      supplier ||= SuppliersPlugin::Supplier.create_dummy :consumer => consumer, :name => supplier_name
+      supplier ||= SuppliersPlugin::Supplier.create_dummy consumer: consumer, name: supplier_name
 
       products.each do |attributes|
-        product = supplier.profile.products.find_by_name attributes[:name].gsub('"', '&quot;')
+        product = supplier.profile.products.find_by_name attributes[:name]
         product ||= supplier.profile.products.build attributes
         product.update_attributes! attributes
         # this is necessary on update
