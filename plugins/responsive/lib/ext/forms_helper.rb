d99a0f784d7af54c46ff005c551229c424b5c784
Braulio Bhavamitra <braulio@eita.org.br>
2015-06-23 11:09:17 -0300

    responsive: support option on submit_button

--- plugins/responsive/lib/ext/forms_helper.rb
+++ plugins/responsive/lib/ext/forms_helper.rb
@@ -29,22 +29,17 @@ module FormsHelper
     def submit_button type, label, html_options = {}
       return super unless theme_responsive?
 
-      bt_cancel = html_options[:cancel] ? button(:cancel, _('Cancel'), html_options[:cancel]) : ''
-
-      button_size = html_options[:size] || 'default'
-      size_class = if button_size == 'default' then '' else 'btn-'+button_size end
-      html_options.delete :size if html_options[:size]
-
       html_options[:class] = [html_options[:class], 'submit'].compact.join(' ')
 
-      the_class = "btn #{size_class} btn-default with-text icon-#{type}"
-      if html_options.has_key?(:class)
-        the_class << ' ' << html_options[:class]
-      end
+      option = html_options.delete(:option) || 'default'
+      size = html_options.delete(:size) || 'default'
+      the_class = "with-text btn btn-#{size} btn-#{option} icon-#{type}"
+      the_class << ' ' << html_options[:class] if html_options.has_key?(:class)
 
-      html_options.delete(:cancel)
+      bt_cancel = html_options[:cancel] ? button(:cancel, _('Cancel'), html_options[:cancel]) : ''
+      html_options.delete :cancel
+
       bt_submit = button_tag(label, html_options.merge(class: the_class))
-
       bt_submit + bt_cancel
     end
 
