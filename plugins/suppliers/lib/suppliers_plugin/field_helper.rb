ffe66897d9d5a194554450fde487ce18d4b72c28
Braulio Bhavamitra <braulio@eita.org.br>
2015-06-23 11:26:06 -0300

    orders: use bootstrap's help-block

--- plugins/suppliers/lib/suppliers_plugin/field_helper.rb
+++ plugins/suppliers/lib/suppliers_plugin/field_helper.rb
@@ -6,11 +6,11 @@ module SuppliersPlugin::FieldHelper
     help = options.delete(:help)
 
     field_label = (form ? form.label(field, label) : label_tag(field, label))
-    field_help = help.blank? ? '' : content_tag('div', help, :class => 'field-help')
-    field_box = content_tag('div', field_html, :class => 'field-box')
+    field_help = help.blank? ? '' : content_tag('div', help, class: 'field-help')
+    field_box = content_tag('div', field_html, class: 'field-box')
 
     content_tag('div', field_label + field_help + field_box,
-                options.merge(:class => options[:class].to_s + ' field'))
+                options.merge(class: options[:class].to_s + ' field'))
   end
 
 end
