4a5eee12e9f63e09c916abd34556550f03b999f7
Braulio Bhavamitra <braulio@eita.org.br>
2015-06-29 20:07:58 -0300

    fbes: better query and include consumers' coops

--- plugins/fbes/controllers/public/fbes_plugin/responsa_controller.rb
+++ plugins/fbes/controllers/public/fbes_plugin/responsa_controller.rb
@@ -1,31 +1,37 @@
 class FbesPlugin::ResponsaController < PublicController
 
-  def enterprises
-      @enterprises = environment.enterprises.enabled
+  def initiatives
+    @last_updated_on = params[:last_updated_on] || 24
+    @last_updated_on = @last_updated_on.months.ago
 
-      @page = (params[:page] || 1).to_i
-      @per_page = if params[:per_page] == 'all' then 999999 else (params[:per_page] || 20).to_i end
-      @enterprises = @enterprises.paginate page: @page, per_page: @per_page
+    @enterprises = environment.enterprises.enabled.joins(:products, :articles).
+      where('profiles.updated_at > ? OR products.updated_at > ? OR articles.updated_at > ?', @last_updated_on, @last_updated_on, @last_updated_on).
+      uniq.group('profiles.id')
+    @consumers_coops = environment.communities.joins(:orders).
+      where('orders_plugin_orders.updated_at > ?', @last_updated_on).
+      uniq.group('profiles.id')
 
-      @json = @enterprises.map do |e|
-        {
-          local_id: e.id,
-          url: url_for(e.url),
-          title: e.short_name(null),
-          description: e.description,
-          lat: e.lat,
-          lng: e.lng,
-          address: [e.address, e.address_line2, e.district, e.zip_code].select{ |f| f.present? }.join(', '),
-          city: e.city,
-          state: e.state,
-          country: 'BR',
-          created_at: e.created_at,
-          updated_at: e.updated_at,
-          avatar: if e.image then "#{environment.top_url}#{e.image.public_filename}" else nil end,
-        }
-      end
+    @results = @enterprises + @consumers_coops
 
-      render json: @json
+    @json = @results.map do |e|
+      {
+        local_id: e.id,
+        url: url_for(e.url),
+        title: e.short_name(nil),
+        description: e.description,
+        lat: e.lat,
+        lng: e.lng,
+        address: [e.address, e.address_line2, e.district, e.zip_code].select{ |f| f.present? }.join(', '),
+        city: e.city,
+        state: e.state,
+        country: 'BR',
+        created_at: e.created_at,
+        updated_at: e.updated_at,
+        avatar: if e.image then "#{environment.top_url}#{e.image.public_filename}" else nil end,
+      }
+    end
+
+    render json: Oj.dump(@json, mode: :compat)
   end
 
   protected
