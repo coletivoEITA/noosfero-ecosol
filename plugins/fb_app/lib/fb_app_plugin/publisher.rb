eb220e4351e9fa09dfb4d8531e4a6b5cf521fc42
Braulio Bhavamitra <braulio@eita.org.br>
2015-08-14 22:24:30 -0300

    open_graph: use method argument

--- plugins/fb_app/lib/fb_app_plugin/publisher.rb
+++ plugins/fb_app/lib/fb_app_plugin/publisher.rb
@@ -1,3 +1,10 @@
+# Publishing examples on console
+#   pub=FbAppPlugin::Publisher.default; u=Profile['brauliobo']; a=Article.find 307591
+#   pub.publish_story a, u, :announce_news_from_a_sse_initiative
+#
+#   pub=FbAppPlugin::Publisher.default; u=Profile['brauliobo']; f=FavoriteEnterprisePerson.last
+#   pub.publish_story f, u, :favorite_a_sse_initiative
+#
 class FbAppPlugin::Publisher < OpenGraphPlugin::Publisher
 
   def publish_story object_data, actor, story
