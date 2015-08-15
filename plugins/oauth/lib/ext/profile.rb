abc7188fd8eb1c6dde2429ce9d3a166410dd4b93
Braulio Bhavamitra <braulio@eita.org.br>
2015-08-15 16:30:47 -0300

    oauth: remove in favor of oauth_client

--- plugins/oauth/lib/ext/profile.rb
+++
@@ -1,8 +0,0 @@
-require_dependency 'profile'
-
-class Profile
-
-  has_many :oauth_provider_auths, class_name: 'OauthPlugin::ProviderAuth'
-  has_many :oauth_client_auths, class_name: 'OauthPlugin::ClientAuth', as: :oauth2_resource_owner
-
-end
