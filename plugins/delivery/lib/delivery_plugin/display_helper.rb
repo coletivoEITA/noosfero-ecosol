b6991732376a3585900cfe40d95ce5cda6ffef4b
Braulio Bhavamitra <braulio@eita.org.br>
2015-08-16 12:13:23 -0300

    orders: fix choose of delivery

--- plugins/delivery/lib/delivery_plugin/display_helper.rb
+++ plugins/delivery/lib/delivery_plugin/display_helper.rb
@@ -9,7 +9,7 @@ module DeliveryPlugin::DisplayHelper
     methods = options[:methods] || profile.delivery_methods
 
     options = methods.map do |method|
-      cost = if method.fixed_cost.present? and method.fixed_cost > 0 then float_to_currency_cart(method.fixed_cost, environment) else nil end
+      cost = if method.fixed_cost.present? and method.fixed_cost > 0 then method.fixed_cost_as_currency else nil end
       text = if cost.present? then "#{method.name} (#{cost})" else method.name end
 
       content_tag :option, text, value: method.id,
