e3ee37e4b00175ed63968aec7568e38fbadf6525
Braulio Bhavamitra <braulio@eita.org.br>
2015-08-27 20:34:57 -0300

    orders_cycle: refactor associations and overrides

--- plugins/orders_cycle/models/orders_cycle_plugin/item.rb
+++ plugins/orders_cycle/models/orders_cycle_plugin/item.rb
@@ -2,31 +2,20 @@ class OrdersCyclePlugin::Item < OrdersPlugin::Item
 
   has_one :supplier, through: :product
 
+  # OVERRIDE OrdersPlugin::Item
+  belongs_to :product, class_name: 'OrdersCyclePlugin::OfferedProduct'
+  # DEPRECATED: remove references to this
   belongs_to :offered_product, foreign_key: :product_id, class_name: 'OrdersCyclePlugin::OfferedProduct'
 
   # see also: repeat_product
   attr_accessor :repeat_cycle
 
-  def cycle
-    self.order.cycle
-  end
+  delegate :cycle, to: :order
 
-  # OVERRIDE from OrdersPlugin::Item
+  # OVERRIDE OrdersPlugin::Item
   belongs_to :order, class_name: '::OrdersCyclePlugin::Order', foreign_key: :order_id, touch: true
   belongs_to :sale, class_name: '::OrdersCyclePlugin::Sale', foreign_key: :order_id, touch: true
   belongs_to :purchase, class_name: '::OrdersCyclePlugin::Purchase', foreign_key: :order_id, touch: true
-
-  # OVERRIDE OrdersPlugin::Item
-  has_many :from_products, through: :offered_product
-  has_one :from_product, through: :offered_product
-  has_many :to_products, through: :offered_product
-  has_one :to_product, through: :offered_product
-  has_many :sources_supplier_products, through: :offered_product
-  has_one :sources_supplier_product, through: :offered_product
-  has_many :supplier_products, through: :offered_product
-  has_one :supplier_product, through: :offered_product
-  has_many :suppliers, through: :offered_product
-  has_one :supplier, through: :offered_product
 
   # what items were selled from this item
   def selled_items
