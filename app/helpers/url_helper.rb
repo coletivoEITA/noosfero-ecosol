b1f4d2f37dd06e6884abd127aa0e8037638c9c24
Braulio Bhavamitra <braulio@eita.org.br>
2015-09-01 21:48:18 -0300

    url_helper: convert to string

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -16,7 +16,7 @@ module UrlHelper
     # the request params (see #url_options below)
     host = options[:host]
     if host.blank? or host == environment.default_hostname
-      controller_path = options[:controller].gsub %r{^/}, '' if options[:controller]
+      controller_path = options[:controller].to_s.gsub %r{^/}, '' if options[:controller]
       controller_path ||= self.controller_path
       controller = UrlHelper.controller_path_class[controller_path] ||= "#{controller_path}_controller".camelize.constantize
       profile_needed = controller.profile_needed if controller.respond_to? :profile_needed, true
