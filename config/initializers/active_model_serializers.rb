32fafd0e570be18a8d0d15466581228d06009afb
Braulio Bhavamitra <braulio@eita.org.br>
2015-09-27 20:12:20 -0300

    serializers: use include: '**' by default

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -3,7 +3,7 @@ ActiveModel::Serializer.config.adapter = :attributes
 class ActiveModel::Serializer
 
   def serializable_hash
-    ActiveModel::SerializableResource.new(self.object, serializer: self.class).serializable_hash
+    ActiveModel::SerializableResource.new(self.object, serializer: self.class, include: '**').serializable_hash
   end
 
   alias_method :to_hash, :serializable_hash
