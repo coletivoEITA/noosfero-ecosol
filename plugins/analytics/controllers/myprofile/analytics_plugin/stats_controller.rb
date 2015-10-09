7b2e8860ff9f633990fd34aaedb8a996d93843bb
Braulio Bhavamitra <braulio@eita.org.br>
2015-10-09 11:48:30 -0300

    analytics: view stats in a modal

--- plugins/analytics/controllers/myprofile/analytics_plugin/stats_controller.rb
+++ plugins/analytics/controllers/myprofile/analytics_plugin/stats_controller.rb
@@ -2,18 +2,33 @@ class AnalyticsPlugin::StatsController < MyProfileController
 
   no_design_blocks
 
-  protect 'edit_profile', :profile
   before_filter :skip_page_view
 
   def index
   end
 
   def edit
+    return render_access_denied unless user.has_permission? 'edit_profile', profile
+
     params[:analytics_settings][:enabled] = params[:analytics_settings][:enabled] == 'true'
     params[:analytics_settings][:anonymous] = params[:analytics_settings][:anonymous] == 'true'
     @settings = profile.analytics_settings params[:analytics_settings] || {}
     @settings.save!
     render nothing: true
+  end
+
+  def view
+    params[:profile_ids] ||= [profile.id]
+    ids = params[:profile_ids].map(&:to_i)
+    user.adminships # FIXME just to cache #adminship_ids
+    ids = ids.select{ |id| id.in? user.adminship_ids } unless @user_is_admin
+
+    @profiles = environment.profiles.find ids
+    @user = environment.people.find params[:user_id]
+    @visits = AnalyticsPlugin::Visit.latest.eager_load(:page_views).where(profile_id: ids, analytics_plugin_page_views: {user_id: @user.id})
+
+    render partial: 'table', locals: {visits: @visits}
+
   end
 
   protected
