0ae791e29614c4c8e75207e9bc8d32c9be656e43
Braulio Bhavamitra <braulio@eita.org.br>
2016-03-12 09:02:10 -0300

    orders: add missing extend for migrations

--- plugins/orders/models/orders_plugin/sale.rb
+++ plugins/orders/models/orders_plugin/sale.rb
@@ -18,6 +18,7 @@ class OrdersPlugin::Sale < OrdersPlugin::Order
     self.total_price_consumer_ordered
   end
 
+  extend CurrencyHelper::ClassMethods
   has_number_with_locale :purchase_quantity_total
   has_currency :purchase_price_total
 
