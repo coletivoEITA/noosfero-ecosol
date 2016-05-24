993068ef08f90a8490c87c286f31ff42c618714e
Braulio Bhavamitra <braulio@eita.org.br>
2016-05-24 12:44:16 -0300

    url_helper: use module global variable

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,11 +1,7 @@
 module UrlHelper
 
-  extend ActiveSupport::Concern
-
-  included do
-    cattr_accessor :controller_path_class
-    self.controller_path_class = {}
-  end
+  mattr_accessor :controller_path_class
+  self.controller_path_class = {}
 
   def url_for options = {}
     return super unless options.is_a? Hash
@@ -21,7 +17,7 @@ module UrlHelper
     if host.blank? or host == environment.default_hostname
       path = options[:controller].to_s.gsub %r{^/}, '' if options[:controller]
       path ||= self.controller_path
-      controller = self.controller_path_class[path] ||= "#{path}_controller".camelize.constantize
+      controller = UrlHelper.controller_path_class[path] ||= "#{path}_controller".camelize.constantize
       profile_needed = controller.profile_needed if controller.respond_to? :profile_needed, true
       if profile_needed and options[:profile].blank? and params[:profile].present?
         options[:profile] = params[:profile]
@@ -41,7 +37,7 @@ module UrlHelper
     # Only use profile's custom domains for the profiles and the account controllers.
     # This avoids redirects and multiple URLs for one specific resource
     if controller_path = options[:controller] || self.class.controller_path
-      controller = (self.class.controller_path_class[controller_path] ||= "#{controller_path}_controller".camelize.constantize rescue nil)
+      controller = (UrlHelper.controller_path_class[controller_path] ||= "#{controller_path}_controller".camelize.constantize rescue nil)
       profile_needed = controller.profile_needed rescue false
       if controller and not profile_needed and not controller == AccountController
         options.merge! :host => environment.default_hostname, :only_path => false
