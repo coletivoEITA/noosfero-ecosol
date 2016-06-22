8ac9d7812a6af369fd78a92e96d007d5d3ba7000
Braulio Bhavamitra <braulio@eita.org.br>
2016-06-22 17:26:49 -0300

    search: Move region includes to profile to workaround rails bug

--- plugins/products/controllers/public/products_plugin/search_controller.rb
+++ plugins/products/controllers/public/products_plugin/search_controller.rb
@@ -5,10 +5,10 @@ module ProductsPlugin
 
     def products
       @scope = @environment.products.enabled.is_public.includes(
-        :product_category, :unit, :region, :image, {inputs: [:product_category]},
+        :product_category, :unit, :image, {inputs: [:product_category]},
         {product_qualifiers: [:qualifier, :certifier]},
         {price_details: [:production_cost]},
-        {profile: [:domains]},
+        {profile: [:region, :domains]},
       )
       full_text_search
     end
