bca19b43f986a1d554357ec73f950aa56615c304
Hugo Melo <hugo@riseup.net>
2016-09-28 17:16:53 -0300

    payments: seed default payment types

--- plugins/payments/db/migrate/20160922122001_create_payment_type.rb
+++ plugins/payments/db/migrate/20160922122001_create_payment_type.rb
@@ -1,9 +1,13 @@
 class CreatePaymentType < ActiveRecord::Migration
-  def change
+  def up
     create_table :payments_plugin_payment_types do |t|
       t.string :slug
       t.string :name
-      t.text :description
+      t.text :description, default: ""
     end
+    execute "INSERT INTO payments_plugin_payment_types (slug, name) VALUES ('money', 'money'), ('check', 'check'), ('credit_card', 'credit_card'), ('bank_transfer', 'bank_transfer'), ('boleto', 'boleto');"
+  end
+  def down
+    drop_table :payments_plugin_payment_types
   end
 end
