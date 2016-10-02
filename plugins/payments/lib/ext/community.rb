4b8fe66d4167b9ac522c6468add4cd8b7eb514b6
Hugo Melo <hugo@riseup.net>
2016-10-02 19:03:53 -0300

    payments: adjust profile configuration

---
+++ plugins/payments/lib/ext/community.rb
@@ -0,0 +1,8 @@
+require_dependency 'community'
+
+class Community
+
+  attr_accessible :payment_method_ids
+
+end
+
--- plugins/payments/models/payments_plugin/payment_method.rb
+++ plugins/payments/models/payments_plugin/payment_method.rb
@@ -3,6 +3,7 @@ class PaymentsPlugin::PaymentMethod < ApplicationRecord
   attr_accessible :slug, :name, :description
 
   has_many :payments
+  has_and_belongs_to_many :profiles
 
   def self.all_in_a_list
     all.map{|p| [p.name, p.id]}
