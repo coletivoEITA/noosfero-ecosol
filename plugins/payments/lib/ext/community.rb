d3b99e83880037198d8935f2a2f73cad2445eaaa
Hugo Melo <hugo@riseup.net>
2016-10-25 15:18:25 -0200

    payments: adjust models

--- plugins/payments/lib/ext/community.rb
+++ plugins/payments/lib/ext/community.rb
@@ -4,8 +4,5 @@ class Community
 
   attr_accessible :payment_method_ids
 
-  def payment_methods_list
-    self.payment_methods.each_with_object({}){|pm,h| h[pm.slug] = pm.name }
-  end
 end
 
--- plugins/payments/models/payments_plugin/payment.rb
+++ plugins/payments/models/payments_plugin/payment.rb
@@ -2,11 +2,16 @@ class PaymentsPlugin::Payment < ApplicationRecord
   attr_accessible :orders_plugin_order_id, :profile_id, :payment_method_id, :operator_id, :value, :description
 
   # ASSOCIATIONS
-  belongs_to :order
+  belongs_to :order, class_name: OrdersPlugin::Order, foreign_key: :orders_plugin_order_id
   belongs_to :profile
   belongs_to :operator, class_name: "Profile"
-  belongs_to :payment_method
+  belongs_to :payment_method, class_name: PaymentsPlugin::PaymentMethod
 
+  validates_presence_of :profile_id
+  validates_presence_of :payment_method_id
+  validates_presence_of :operator_id
+  validates_presence_of :value
+  validates_presence_of :orders_plugin_order_id
 
   # FINANCIAL CALLBACKS
   if defined? FinancialPlugin
@@ -21,16 +26,18 @@ class PaymentsPlugin::Payment < ApplicationRecord
   def create_transaction
     self.create_financial_transaction(
       profile_id: self.profile_id,
-      quantity: self.value,
+      operator_id: self.operator_id,
+      value: self.value,
       description: "new payment"
     )
   end
 
   def update_transaction
-   if self.transaction.value != self.value
-      self.transaction.value = self.value
-      self.transaction.save
-   end
+    self.create_transaction unless self.financial_transaction
+    if self.financial_transaction.value != self.value
+      self.financial_transaction.value = self.value
+      self.financial_transaction.save
+    end
   end
 
 end
