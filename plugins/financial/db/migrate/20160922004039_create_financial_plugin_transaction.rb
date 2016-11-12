85a7536fd3e6f271695709247eb4fc1c0221972f
Hugo Melo <hugo@riseup.net>
2016-11-12 10:42:44 -0200

    financial: recreate transactions structure

--- plugins/financial/db/migrate/20160922004039_create_financial_plugin_transaction.rb
+++ plugins/financial/db/migrate/20160922004039_create_financial_plugin_transaction.rb
@@ -1,16 +1,21 @@
 class CreateFinancialPluginTransaction < ActiveRecord::Migration
   def change
     create_table :financial_plugin_transactions do |t|
-      t.references :profile, index: true, foreign_key: true
-      t.references :context, polymorphic: true
-      t.references :target,  polymorphic: true
-      t.references :source,  polymorphic: true
-      t.integer    :operator_id, index: true
-      t.decimal    :value
+
+      t.references :target,      polymorphic: true
+      t.references :target_profile,    index: true, default: nil
+      t.integer    :origin_id,         index: true, default: nil
+
+      t.integer    :order_id,          index: true, default: nil
+      t.integer    :payment_id,        index: true, default: nil
+      t.integer    :payment_method_id, index: true, default: nil
+      t.integer    :operator_id,       index: true, default: nil
+
       t.text       :description
-      t.string     :category
       t.string     :direction
       t.decimal    :balance
+      t.decimal    :value
+      t.datetime   :date
       t.timestamps
     end
   end
--- plugins/financial/models/financial_plugin/transaction.rb
+++ plugins/financial/models/financial_plugin/transaction.rb
@@ -1,13 +1,15 @@
 class FinancialPlugin::Transaction < ApplicationRecord
-  attr_accessible :profile_id, :target_id, :operator_id, :description, :category, :value, :direction, :balance
+  attr_accessible :origin_id, :target, :target_profile, :target_profile_id, :operator_id, :description, :date, :value, :direction, :balance
 
-  belongs_to :source,  polymorphic: true
-  belongs_to :context, polymorphic: true
-  belongs_to :target,  polymorphic: true
+  belongs_to :target,         polymorphic: true
+  belongs_to :target_profile, class_name: "Profile"
+  belongs_to :order,          class_name: "OrdersPlugin::Order"
+  belongs_to :payment,        class_name: "PaymentsPlugin::Payment"
+  belongs_to :operator,       class_name: "Profile"
   belongs_to :profile
-  belongs_to :target,   class_name: "Profile"
-  belongs_to :operator, class_name: "Profile"
 
   scope :outputs, -> { where direction: :out }
   scope :inputs,  -> { where direction: :in }
+
+  validates_presence_of :direction, :value, :date
 end
