67d2ad2c105c0a357c2047a4523a89009759060d
Hugo Melo <hugo@riseup.net>
2016-12-13 10:59:54 -0200

    products: fix html_safe

--- plugins/products/lib/products_plugin/enterprise_homepage_helper.rb
+++ plugins/products/lib/products_plugin/enterprise_homepage_helper.rb
@@ -12,16 +12,16 @@ module ProductsPlugin
         [ _('Economic activity:'), :economic_activity ]
       ].each { | name, att |
         if profile.send( att ) and not profile.send( att ).blank?
-          data << content_tag( 'li', content_tag('strong', name) +' '+ profile.send( att ).to_s ) +"\n"
+          data << content_tag( 'li', content_tag('strong', name) +' '+ profile.send( att ).to_s )
         end
       }
       if profile.respond_to?(:distance) and !profile.distance.nil?
         data << content_tag( 'li',
                              content_tag('strong',_('Distance:')) +' '+
                              "%.2f%" % profile.distance
-                           ) + "\n"
+                           ).html_safe
       end
-      content_tag('div', content_tag('ul', data), class: 'enterprise-info')
+      content_tag('div', content_tag('ul', data.html_safe), class: 'enterprise-info')
     end
   end
 end
