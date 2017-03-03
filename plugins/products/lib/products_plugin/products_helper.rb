73eaafbb21565782df699c7b10e63b509be51582
Hugo Melo <hugo@riseup.net>
2017-03-03 10:54:28 -0300

    Fix html_safe issues

--- plugins/products/lib/products_plugin/products_helper.rb
+++ plugins/products/lib/products_plugin/products_helper.rb
@@ -179,7 +179,7 @@ module ProductsPlugin::ProductsHelper
   def cancel_edit_product_link(product, field, html_options = {})
     return '' unless (user && user.has_permission?('products_plugin/page', profile))
     button_to_function(:cancel, _('Cancel'), nil, html_options) do |page|
-      page.replace_html "product-#{field}", CGI::escapeHTML(render "products_plugin/page/display_#{field}", product: product)
+      page.replace_html "product-#{field}", (render "products_plugin/page/display_#{field}", product: product)
     end
   end
 
