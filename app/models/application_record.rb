5bcc03777314b710b89a5da89f2e7e3a5a8da2a1
Hugo Melo <hugo@riseup.net>
2017-03-03 15:23:06 -0300

    Revert "application_record: fix infinite loop"
    This reverts commit a1755f5af932e62761cf76fd6270641fe1bb9b64.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -19,8 +19,9 @@ class ApplicationRecord < ActiveRecord::Base
     self.class.name.underscore
   end
 
+  alias :meta_cache_key :cache_key
   def cache_key
-    key = [Noosfero::VERSION]
+    key = [Noosfero::VERSION, meta_cache_key]
     key.unshift ApplicationRecord.connection.schema_search_path
     key.join('/')
   end
